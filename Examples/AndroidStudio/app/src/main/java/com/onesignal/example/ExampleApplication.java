fbf105a2668a3e8f8f9ee483a35f79dfd203e3b2
Josh Kasten <Josh@onesignal.com>
2015-12-11 23:07:06 -0800

    Updated Android Studio example to SDK 2.0

---
+++ Examples/AndroidStudio/app/src/main/java/com/onesignal/example/ExampleApplication.java
@@ -0,0 +1,53 @@
+package com.onesignal.example;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.onesignal.OneSignal;
+
+import org.json.JSONObject;
+
+public class ExampleApplication extends Application {
+
+   @Override
+   public void onCreate() {
+      super.onCreate();
+
+      // Logging set to help debug issues, remove before releasing your app.
+      OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.WARN);
+
+      OneSignal.startInit(this)
+          .setNotificationOpenedHandler(new ExampleNotificationOpenedHandler())
+          .init();
+   }
+
+
+   private class ExampleNotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
+      /**
+       * Callback to implement in your app to handle when a notification is opened from the Android status bar or
+       * a new one comes in while the app is running.
+       * This method is located in this Application class as an example, you may have any class you wish implement NotificationOpenedHandler and define this method.
+       *
+       * @param message        The message string the user seen/should see in the Android status bar.
+       * @param additionalData The additionalData key value pair section you entered in on onesignal.com.
+       * @param isActive       Was the app in the foreground when the notification was received.
+       */
+      @Override
+      public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
+         String additionalMessage = "";
+
+         try {
+            if (additionalData != null) {
+               if (additionalData.has("actionSelected"))
+                  additionalMessage += "Pressed ButtonID: " + additionalData.getString("actionSelected");
+
+               additionalMessage = message + "\nFull additionalData:\n" + additionalData.toString();
+            }
+
+         Log.d("OneSignalExample", "message:\n" + message + "\nadditionalMessage:\n" + additionalMessage);
+         } catch (Throwable t) {
+            t.printStackTrace();
+         }
+      }
+   }
+}
