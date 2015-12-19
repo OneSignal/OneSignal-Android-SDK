fe1e443918ccfb14c050422b96727529b168f92f
Josh Kasten <Josh@onesignal.com>
2015-12-18 23:30:02 -0800

    Compatibility fix for GMS library 8.1.0 and newer.
    * Fixed compatibility issues when using GMS (Google Play services library) 8.1.0 or newer in your app.
       - Also insured backwards compatibility to 7.0.0.

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/LocationGMS.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/LocationGMS.java
@@ -1,16 +1,40 @@
+/**
+ * Modified MIT License
+ *
+ * Copyright 2015 OneSignal
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * 1. The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * 2. All copies of substantial portions of the Software may only be used in connection
+ * with services provided by OneSignal.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.onesignal;
 
 import com.onesignal.AndroidSupportV4Compat.ContextCompat;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -22,7 +46,7 @@ import java.util.Arrays;
 import java.util.List;
 
 class LocationGMS {
-   private static GoogleApiClient mGoogleApiClient;
+   private static GoogleApiClientCompatProxy mGoogleApiClient;
    static String requestPermission;
 
    private static ActivityLifecycleHandler.ActivityAvailableListener activityAvailableListener;
@@ -78,11 +102,13 @@ class LocationGMS {
 
    static void startGetLocation() {
       GoogleApiClientListener googleApiClientListener = new GoogleApiClientListener();
-      mGoogleApiClient = new GoogleApiClient.Builder(OneSignal.appContext)
+      GoogleApiClient googleApiClient = new GoogleApiClient.Builder(OneSignal.appContext)
           .addApi(LocationServices.API)
           .addConnectionCallbacks(googleApiClientListener)
           .addOnConnectionFailedListener(googleApiClientListener)
           .build();
+      mGoogleApiClient = new GoogleApiClientCompatProxy(googleApiClient);
+
       mGoogleApiClient.connect();
    }
 
@@ -99,7 +125,7 @@ class LocationGMS {
       public void onConnected(Bundle bundle) {
          PermissionsActivity.answered = false;
 
-         Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+         Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient.realInstance());
          // Coarse always gives out 14 digits and has an accuracy 2000. Always rounding to 7 as this is what fine returns.
          if (location != null)
             locationHandler.complete(new BigDecimal(location.getLatitude()).setScale(7, RoundingMode.HALF_UP).doubleValue(), new BigDecimal(location.getLongitude()).setScale(7, RoundingMode.HALF_UP).doubleValue());
--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -162,7 +162,7 @@ public class OneSignal {
    private static TrackGooglePurchase trackGooglePurchase;
    private static TrackAmazonPurchase trackAmazonPurchase;
 
-   public static final String VERSION = "020001";
+   public static final String VERSION = "020002";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
