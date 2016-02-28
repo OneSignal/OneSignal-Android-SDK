b36f98c39fe293d5cbccffa7cfc1ce2d84011c85
Josh Kasten <Josh@onesignal.com>
2016-02-28 01:34:49 -0800

    Version bump to 2.1.2

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -155,7 +155,7 @@ public class OneSignal {
    private static TrackGooglePurchase trackGooglePurchase;
    private static TrackAmazonPurchase trackAmazonPurchase;
 
-   public static final String VERSION = "020101";
+   public static final String VERSION = "020102";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
