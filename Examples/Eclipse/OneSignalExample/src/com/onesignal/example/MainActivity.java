f200921a8cef1e3205161831eeee2756a82c8b78
Josh Kasten <Josh@onesignal.com>
2016-08-08 19:35:47 -0700

    Updated examples for 3.0 release.

--- Examples/Eclipse/OneSignalExample/src/com/onesignal/example/MainActivity.java
+++ Examples/Eclipse/OneSignalExample/src/com/onesignal/example/MainActivity.java
@@ -1,7 +1,7 @@
 /**
  * Modified MIT License
- * 
- * Copyright 2015 OneSignal
+ *
+ * Copyright 2016 OneSignal
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -9,13 +9,13 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * 1. The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * 2. All copies of substantial portions of the Software may only be used in connection
  * with services provided by OneSignal.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,18 +27,13 @@
 
 package com.onesignal.example;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.onesignal.OneSignal;
-import com.onesignal.OneSignal.NotificationOpenedHandler;
 import com.onesignal.OneSignal.IdsAvailableHandler;
 
 public class MainActivity extends Activity {
@@ -49,13 +44,6 @@ public class MainActivity extends Activity {
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-      currentActivity = this;
-
-      // Enable Logging below to debug issues. (LogCat level, Visual level);
-      // OneSignal.setLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
-
-      // Pass in your app's Context
-      OneSignal.startInit(this).setNotificationOpenedHandler(new ExampleNotificationOpenedHandler()).init();
    }
 
    // activity_main.xml defines the link to this method from the button.
@@ -78,58 +66,5 @@ public class MainActivity extends Activity {
             Log.i("OneSignalExample", labelStr + "\n");
          }
       });
-   }
-
-   // NotificationOpenedHandler is implemented in its own class instead of adding implements to MainActivity so we don't hold on to a reference of our first activity if it gets recreated.
-   private class ExampleNotificationOpenedHandler implements NotificationOpenedHandler {
-      /**
-       * Called when a notification is opened from the Android status bar or a new one comes in while the app is in focus.
-       *
-       * @param message
-       *           The message string the user seen/should see in the Android status bar.
-       * @param additionalData
-       *           The additionalData key value pair section you entered in on onesignal.com.
-       * @param isActive
-       *           Was the app in the foreground when the notification was received.
-       */
-      @Override
-      public void notificationOpened(String message, JSONObject additionalData, boolean isActive) {
-         String messageTitle = "OneSignal Example", messageBody = message;
-
-         try {
-            if (additionalData != null) {
-               if (additionalData.has("title"))
-                  messageTitle = additionalData.getString("title");
-               if (additionalData.has("actionSelected"))
-                  messageBody += "\nPressed ButtonID: " + additionalData.getString("actionSelected");
-
-               messageBody = message + "\n\nFull additionalData:\n" + additionalData.toString();
-            }
-         } catch (JSONException e) {
-         }
-
-         SafeAlertDialog(messageTitle, messageBody);
-      }
-   }
-
-   // AlertDialogs do not show on Android 2.3 if they are trying to be displayed while the activity is pause.
-   // We sleep for 500ms to wait for the activity to be ready before displaying.
-   private static void SafeAlertDialog(final String msgTitle, final String msgBody) {
-      new Thread(new Runnable() {
-         public void run() {
-				try {Thread.sleep(500);} catch(Throwable t) {}
-
-            MainActivity.currentActivity.runOnUiThread(new Runnable() {
-               public void run() {
-                  new AlertDialog.Builder(MainActivity.currentActivity)
-                     .setTitle(msgTitle)
-                     .setMessage(msgBody)
-                     .setCancelable(true)
-                     .setPositiveButton("OK", null)
-                     .create().show();
-               }
-            });
-         }
-      }).start();
    }
 }
