6c87c1be8db5c2537f0942e2c53d22ec30e16bb5
Josh Kasten <Josh@onesignal.com>
2016-08-31 14:30:29 -0700

    Added removeNotificationReceivedHandler method for other SDKs to use

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -167,7 +167,7 @@ public class OneSignal {
    private static TrackGooglePurchase trackGooglePurchase;
    private static TrackAmazonPurchase trackAmazonPurchase;
 
-   public static final String VERSION = "030100";
+   public static final String VERSION = "030101";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
@@ -1261,6 +1261,10 @@ public class OneSignal {
 
    public static void removeNotificationOpenedHandler() {
       mInitBuilder.mNotificationOpenedHandler = null;
+   }
+
+   public static void removeNotificationReceivedHandler() {
+      mInitBuilder.mNotificationReceivedHandler = null;
    }
 
    static long GetUnsentActiveTime() {
