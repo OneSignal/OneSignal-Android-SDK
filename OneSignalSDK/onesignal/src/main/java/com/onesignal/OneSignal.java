85868c53febea480768eb56da8efc131136bf6c2
Josh Kasten <Josh@onesignal.com>
2016-10-19 11:32:52 -0700

    Increased minor version number and created release jar

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -167,7 +167,7 @@ public class OneSignal {
    private static TrackGooglePurchase trackGooglePurchase;
    private static TrackAmazonPurchase trackAmazonPurchase;
 
-   public static final String VERSION = "030301";
+   public static final String VERSION = "030400";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
