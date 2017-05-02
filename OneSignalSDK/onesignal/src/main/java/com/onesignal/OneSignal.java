fe5f887c74e6f1971faa577e0f72126d2c65f85f
Josh Kasten <Josh@onesignal.com>
2017-05-01 17:19:15 -0700

    3.5.3 Release commit - limited dependency ranges

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -181,7 +181,7 @@ public class OneSignal {
    private static TrackGooglePurchase trackGooglePurchase;
    private static TrackAmazonPurchase trackAmazonPurchase;
 
-   public static final String VERSION = "030502";
+   public static final String VERSION = "030503";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
