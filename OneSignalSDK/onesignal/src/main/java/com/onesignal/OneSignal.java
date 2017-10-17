516ec74e2a9f4f39191bb2c648d009658b2844f1
Alamgir Mand <alamgir@onesignal.com>
2017-10-17 13:29:42 -0700

    Setting version to 3.6.4

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -310,7 +310,7 @@ public class OneSignal {
    private static TrackGooglePurchase trackGooglePurchase;
    private static TrackAmazonPurchase trackAmazonPurchase;
 
-   public static final String VERSION = "030603";
+   public static final String VERSION = "030604";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
