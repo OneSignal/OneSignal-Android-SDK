663a1d12b6deed5fa4ce7c17f582c49a0c06e810
Alamgir Mand <alamgir@onesignal.com>
2017-10-16 17:41:01 -0700

    Adding a comment to designate why we chose M

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/NotificationRestorer.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/NotificationRestorer.java
@@ -104,6 +104,7 @@ class NotificationRestorer {
               NotificationTable.COLUMN_NAME_IS_SUMMARY + " = 0");
 
       //retrieve the list of notifications that are currently in the shade
+      //this is used to prevent notifications from being restored twice in M and newer
       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
          NotificationManager notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
          if(notifManager != null) {
