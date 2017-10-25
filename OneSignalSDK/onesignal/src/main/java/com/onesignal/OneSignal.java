d0541d9fef87a45287378220d6567588c0cd814e
Alamgir Mand <alamgir@onesignal.com>
2017-10-25 15:15:07 -0700

    Incrementing SDK version to 3.6.5

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -311,7 +311,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   public static final String VERSION = "030604";
+   public static final String VERSION = "030605";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
