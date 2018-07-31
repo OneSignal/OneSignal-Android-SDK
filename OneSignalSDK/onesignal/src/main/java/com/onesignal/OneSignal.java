58827dc0658e01756ca5d1912fc47f14595aaea8
Brad Hesse <nightsd01@gmail.com>
2018-07-31 15:25:11 -0700

    3.10.0 Release Commit
    • Adds a ChangeTagsUpdateHandler to allow for callbacks when sending/deleting tags
    • Publicly exposes onesignalLog() method so that wrapper SDK's can use it.
    • Adds a getter method for requiresUserPrivacyConsent
    • Corrects wakeful service GCM fallback

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -361,7 +361,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   public static final String VERSION = "030903";
+   public static final String VERSION = "031000";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
