6808d44a0a5c78892e243476a2028e5e6e68845a
Brad Hesse <nightsd01@gmail.com>
2018-08-07 14:39:57 -0700

    Improve Android SDK Demo Project
    • We have several demo projects for Android, but the default demo project has few options
    • This commit adds multiple buttons to the demo to test several different aspects of the SDK without having to open a different project or deal with modules/etc.

--- OneSignalSDK/app/src/main/java/com/onesignal/example/MainActivity.java
+++ OneSignalSDK/app/src/main/java/com/onesignal/example/MainActivity.java
@@ -28,13 +28,18 @@
 package com.onesignal.example;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
 
 import com.onesignal.*;
 import com.onesignal.OneSignal.NotificationOpenedHandler;
@@ -42,40 +47,53 @@ import com.onesignal.OneSignal.NotificationOpenedHandler;
 import com.onesignal.example.iap.IabHelper;
 import com.onesignal.example.iap.IabResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Locale;
+import java.util.ArrayList;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OSEmailSubscriptionObserver, OSPermissionObserver, OSSubscriptionObserver, NotificationOpenedHandler, OneSignal.NotificationReceivedHandler {
 
    IabHelper mHelper;
+
+   private int[] interactiveViewIds = new int[] {R.id.subscribe, R.id.unsubscribe, R.id.sendTags, R.id.getTags, R.id.logoutEmail, R.id.setEmail, R.id.emailTextView};
+
+   private TextView debugTextView;
+   private TextView emailTextView;
+   private Button consentButton;
+   private Button setEmailButton;
+   private Button logoutEmailButton;
+
+   private int sendTagsCounter = 1;
+   private boolean addedObservers = false;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
-      Log.e("tet", Locale.getDefault().getLanguage());
+      this.consentButton = (Button)this.findViewById(R.id.consentButton);
+      this.setEmailButton = (Button)this.findViewById(R.id.setEmail);
+      this.logoutEmailButton = (Button)this.findViewById(R.id.logoutEmail);
 
-      OneSignal.setInFocusDisplaying(OneSignal.OSInFocusDisplayOption.InAppAlert);
-      OneSignal.sendTag("test3", "test7");
-      //OneSignal.setSubscription(false);
+      if (OneSignal.requiresUserPrivacyConsent()) {
+         //disable all interactive views except consent button
+         this.changeInteractiveViewsEnabled(false);
+         consentButton.setText("Provide Consent");
+      } else {
+         consentButton.setText("Revoke Consent");
 
-      OneSignal.syncHashedEmail("test@onesignal.com");
+         this.addObservers();
+      }
 
-//        OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
-//            @Override
-//            public void idsAvailable(String userId, String registrationId) {
-//                Log.i("OneSignal Example:", "UserID: " + userId + ", RegId: " + (registrationId != null ? registrationId : "null"));
-//
-//                try {
-//                    OneSignal.postNotification(new JSONObject("{'contents': {'en':'Test Message'}, 'include_player_ids': ['" + userId + "']}"), null);
-//                    //OneSignal.postNotification(new JSONObject("{'contents': {'en':'Test Message'}, 'include_player_ids': ['" + "86480bb0-ef9a-11e4-8cf1-000c29917011', '2def6d7a-4395-11e4-890a-000c2940e62c" + "']}"), null);
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
+      this.debugTextView = this.findViewById(R.id.debugTextView);
+      this.emailTextView = this.findViewById(R.id.emailTextView);
 
+      OSPermissionSubscriptionState state = OneSignal.getPermissionSubscriptionState();
+
+      this.didGetEmailStatus(state.getEmailSubscriptionStatus().getSubscribed());
 
       // compute your public key and store it in base64EncodedPublicKey
       mHelper = new IabHelper(this, "sdafsfds");
@@ -88,6 +106,67 @@ public class MainActivity extends Activity {
             // Hooray, IAB is fully set up!
          }
       });
+
+
+   }
+
+   private void updateTextView(final String newText) {
+      //ensure that we only update the text view from the main thread.
+
+      Handler mainHandler = new Handler(Looper.getMainLooper());
+
+      Runnable runnable = new Runnable() {
+         @Override
+         public void run() {
+            debugTextView.setText(newText);
+         }
+      };
+
+      mainHandler.post(runnable);
+   }
+
+   private void addObservers() {
+      this.addedObservers = true;
+
+      Log.d("onesignal", "adding observers");
+
+      OneSignal.addEmailSubscriptionObserver(this);
+
+      OneSignal.addSubscriptionObserver(this);
+
+      OneSignal.addPermissionObserver(this);
+   }
+
+   @Override
+   public void onOSEmailSubscriptionChanged(OSEmailSubscriptionStateChanges stateChanges) {
+      updateTextView("Email Subscription State Changed: " + stateChanges.toString());
+
+      didGetEmailStatus(stateChanges.getTo().getSubscribed());
+   }
+
+   @Override
+   public void onOSSubscriptionChanged(OSSubscriptionStateChanges stateChanges) {
+      updateTextView("Push Subscriuption State Changed: " + stateChanges.toString());
+   }
+
+   @Override
+   public void onOSPermissionChanged(OSPermissionStateChanges stateChanges) {
+      updateTextView("Permission State Changed: " + stateChanges.toString());
+   }
+
+   @Override
+   public void notificationOpened(OSNotificationOpenResult result) {
+      updateTextView("Opened Notification: " + result.toString());
+   }
+
+   @Override
+   public void notificationReceived(OSNotification notification) {
+      updateTextView("Received Notification: " + notification.toString());
+   }
+
+   public void didGetEmailStatus(boolean hasEmailUserId) {
+      this.setEmailButton.setEnabled(!hasEmailUserId);
+      this.logoutEmailButton.setEnabled(hasEmailUserId);
    }
 
    public void onSubscribeClicked(View v) {
@@ -99,32 +178,94 @@ public class MainActivity extends Activity {
       nMgr.cancelAll();
    }
 
+   private void changeInteractiveViewsEnabled(boolean enabled) {
+      for (int viewId : this.interactiveViewIds) {
+         this.findViewById(viewId).setEnabled(enabled);
+      }
+   }
+
    public void onUnsubscribeClicked(View v) {
       OneSignal.setSubscription(false);
+
+      this.debugTextView.setText("Unsubscribed");
    }
 
 
    public void onSendTagsClicked(View v) {
-      OneSignal.sendTags("{\"test\":\"value\"}");
+      try {
+         OneSignal.sendTags(new JSONObject("{\"counter\" : " + String.valueOf(sendTagsCounter) + ", \"test_value\" : \"test_key\"}"), new OneSignal.ChangeTagsUpdateHandler() {
+            @Override
+            public void onSuccess(JSONObject tags) {
+               updateTextView("Successfully sent tags: " + tags.toString());
+            }
+
+            @Override
+            public void onFailure(OneSignal.SendTagsError error) {
+               updateTextView("Failed to send tags: " + error.toString());
+            }
+         });
+
+         sendTagsCounter++;
+      } catch (JSONException exception) {
+         updateTextView("Failed to serialize tags into JSON: " + exception.getMessage());
+      }
    }
 
    public void onConsentButtonClicked(View v) {
-      OneSignal.provideUserConsent(!OneSignal.userProvidedPrivacyConsent());
+      boolean consentRequired = OneSignal.requiresUserPrivacyConsent();
+
+      //flips consent
+      OneSignal.provideUserConsent(consentRequired);
+      this.changeInteractiveViewsEnabled(consentRequired);
+      this.consentButton.setText(consentRequired ? "Revoke Consent" : "Provide Consent");
+
+      if (consentRequired && this.addedObservers == false)
+         addObservers();
    }
 
-//   @Override
-//   protected void onPause() {
-//      super.onPause();
-//      if (mHelper != null)
-//         mHelper.dispose();
-//   }
-//
-//   @Override
-//   public void onDestroy() {
-//      super.onDestroy();
-//      if (mHelper != null) mHelper.dispose();
-//      mHelper = null;
-//   }
+   public void onGetTagsClicked(View v) {
+      OneSignal.getTags(new OneSignal.GetTagsHandler() {
+         @Override
+         public void tagsAvailable(final JSONObject tags) {
+            updateTextView("Got Tags: " + tags.toString());
+         }
+      });
+   }
+
+   public void onSetEmailClicked(View v) {
+      String email = emailTextView.getText().toString();
+
+      if (email.length() == 0) {
+         Log.d("onesingal", "email not set");
+         return;
+      }
+
+      OneSignal.setEmail(email, new OneSignal.EmailUpdateHandler() {
+         @Override
+         public void onSuccess() {
+            updateTextView("Successfully set email");
+         }
+
+         @Override
+         public void onFailure(OneSignal.EmailUpdateError error) {
+            updateTextView("Failed to set email with error: " + error.getMessage());
+         }
+      });
+   }
+
+   public void onLogoutEmailClicked(View v) {
+      OneSignal.logoutEmail(new OneSignal.EmailUpdateHandler() {
+         @Override
+         public void onSuccess() {
+            updateTextView("Successfully logged out of email");
+         }
+
+         @Override
+         public void onFailure(OneSignal.EmailUpdateError error) {
+            updateTextView("Failed to logout of email with error: " + error.getMessage());
+         }
+      });
+   }
 
    @Override
    public boolean onCreateOptionsMenu(Menu menu) {
