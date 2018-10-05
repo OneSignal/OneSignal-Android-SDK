235e15ebb281e08036c4db100bf1e230e9828f4e
Josh Kasten <josh@onesignal.com>
2018-10-05 16:08:28 -0700

    3.10.2 Release Commit

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -361,7 +361,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   public static final String VERSION = "031001";
+   public static final String VERSION = "031002";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
