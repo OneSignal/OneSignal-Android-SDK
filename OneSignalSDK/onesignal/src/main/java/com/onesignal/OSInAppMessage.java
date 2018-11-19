602e0c4feaf91577f00fee72059659a1f66cf461
Brad Hesse <nightsd01@gmail.com>
2018-11-19 15:10:58 -0800

    In-App Model
    • Adds model classes for OSInAppMessage and OSTrigger

---
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OSInAppMessage.java
@@ -0,0 +1,90 @@
+package com.onesignal;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class OSInAppMessage {
+
+    /**
+     * The unique identifier for this in-app message
+     */
+    @NonNull
+    public String messageId;
+
+    /**
+     * The unique identifier for the in-app message webpage content (HTML)
+     */
+    @NonNull
+    public String contentId;
+
+    /**
+     * An ar1ray of arrays of triggers. The outer array represents AND conditions,
+     * while the inner array represents AND conditions.
+     */
+    @NonNull
+    public ArrayList<ArrayList<OSTrigger>> triggers;
+
+
+    @Nullable
+    public double maxDisplayTime;
+
+    public OSInAppMessage(JSONObject json) throws JSONException {
+
+        // initialize simple root properties
+        this.messageId = json.getString("id");
+        this.contentId = json.getString("content_id");
+        this.maxDisplayTime = json.optDouble("max_display_time");
+
+        // initialize triggers
+        this.triggers = new ArrayList<>();
+
+        JSONArray ors = json.getJSONArray("triggers");
+
+        for (int i = 0; i < ors.length(); i++) {
+            JSONArray ands = ors.getJSONArray(i);
+
+            ArrayList<OSTrigger> converted = new ArrayList();
+
+            for (int j = 0; j < ands.length(); j++)
+                converted.add(new OSTrigger(ands.getJSONObject(j)));
+
+            this.triggers.add(converted);
+        }
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject json = new JSONObject();
+
+        try {
+            json.put("id", this.messageId);
+
+            json.put("content_id", this.contentId);
+
+            json.put("max_display_time", this.maxDisplayTime);
+
+            JSONArray orConditions = new JSONArray();
+
+            for (ArrayList<OSTrigger> andArray : this.triggers) {
+                JSONArray andConditions = new JSONArray();
+
+                for (OSTrigger trigger : andArray)
+                    andConditions.put(trigger.toJSONObject());
+
+                orConditions.put(andConditions);
+            }
+
+            json.put("triggers", orConditions);
+
+        } catch (JSONException exception) {
+            exception.printStackTrace();
+        }
+
+        return json;
+    }
+}
---
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OSTrigger.java
@@ -0,0 +1,97 @@
+package com.onesignal;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class OSTrigger {
+
+    /**
+     * An enumeration of the operators triggers can use
+     */
+    public enum OSTriggerOperatorType {
+        GREATER_THAN(">"),
+        LESS_THAN("<"),
+        EQUAL_TO("=="),
+        NOT_EQUAL_TO("!="),
+        LESS_THAN_OR_EQUAL_TO("<="),
+        GREATER_THAN_OR_EQUAL_TO(">="),
+        EXISTS("exists"),
+        NOT_EXISTS("not_exists"),
+        CONTAINS("contains");
+
+        private String text;
+
+        OSTriggerOperatorType(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return this.text;
+        }
+
+        public static OSTriggerOperatorType fromString(String text) {
+            for (OSTriggerOperatorType type : OSTriggerOperatorType.values()) {
+                if (type.text.equalsIgnoreCase(text)) {
+                    return type;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    /**
+     * The property that this trigger operates on, such as 'game_score'
+     */
+    @NonNull
+    public String property;
+
+
+    /**
+     * The type of operator used to perform the logical equivalence/comparison on,
+     * such as > or <=
+     */
+    @NonNull
+    public OSTriggerOperatorType operatorType;
+
+    /**
+     * Most comparison-based operators have a value to allow for triggers
+     * such as game_score > 30
+     */
+    @Nullable
+    public Object value;
+
+    public OSTrigger(String property, OSTriggerOperatorType type, Object value) {
+        this.property = property;
+        this.operatorType = type;
+        this.value = value;
+    }
+
+    public OSTrigger(JSONObject json) throws JSONException {
+        this.property = json.getString("property");
+        this.operatorType = OSTriggerOperatorType.fromString(json.getString("operator"));
+        this.value = json.getString("value");
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject json = new JSONObject();
+
+        try {
+            json.put("property", this.property);
+
+            json.put("operator", this.operatorType.toString());
+
+            json.putOpt("value", this.value);
+
+        } catch (JSONException exception) {
+            exception.printStackTrace();
+        }
+
+
+        return json;
+    }
+}
