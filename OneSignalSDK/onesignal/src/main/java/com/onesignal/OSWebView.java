41c0248dd9f26be1570e2c31e4fa04910126e0e5
Josh Kasten <josh@onesignal.com>
2019-03-03 23:28:36 -0800

    Fixed miss-alignment with touches to webview
    * Issue was with forwarding onTouchEvent event to the webview from the draggable.
       - This seemed to not account for the margin being added.
       - Removed onTouchEvent and added logic to onInterceptTouchEvent instead.
    * Found correct implementation to preventing scrolling in the webview by overriding overScrollBy and some other methods from our extended class.

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OSWebView.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OSWebView.java
@@ -36,4 +36,22 @@ public class OSWebView extends WebView {
       canvas.clipPath(clipPath);
       super.onDraw(canvas);
    }
+
+   // The method overrides below; overScrollBy, scrollTo, and computeScroll prevent page scrolling
+   @Override
+   public boolean overScrollBy(int deltaX, int deltaY, int scrollX, int scrollY,
+                               int scrollRangeX, int scrollRangeY, int maxOverScrollX,
+                               int maxOverScrollY, boolean isTouchEvent) {
+      return false;
+   }
+
+   @Override
+   public void scrollTo(int x, int y) {
+      // Do nothing
+   }
+
+   @Override
+   public void computeScroll() {
+      // Do nothing
+   }
 }
