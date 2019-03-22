bd974d4f32070064ccdf9b56638372e44ee9512d
Josh Kasten <josh@onesignal.com>
2019-03-22 16:52:50 -0700

    Added tests for SDK-Version HTTP header

--- OneSignalSDK/unittest/src/test/java/com/test/onesignal/RESTClientRunner.java
+++ OneSignalSDK/unittest/src/test/java/com/test/onesignal/RESTClientRunner.java
@@ -29,9 +29,12 @@ package com.test.onesignal;
 
 import com.onesignal.BuildConfig;
 import com.onesignal.MockHttpURLConnection;
-import com.onesignal.OneSignalPackagePrivateHelper;
+import com.onesignal.OneSignal;
+import com.onesignal.OneSignalPackagePrivateHelper.OneSignalRestClient;
 import com.onesignal.ShadowOneSignalRestClientWithMockConnection;
+import com.onesignal.StaticResetHelper;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -40,6 +43,7 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLog;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 @Config(packageName = "com.onesignal.example",
@@ -54,21 +58,58 @@ public class RESTClientRunner {
    public static void setUpClass() throws Exception {
       ShadowLog.stream = System.out;
       TestHelpers.beforeTestSuite();
+      StaticResetHelper.saveStaticValues();
    }
-   
+
    @Before // Before each test
-   public void beforeEachTest() throws Exception {
+   public void beforeEachTest() {
+      TestHelpers.beforeTestInitAndCleanup();
    }
-   
+
+   @AfterClass
+   public static void afterEverything() {
+      StaticResetHelper.restSetStaticFields();
+   }
+
    @Test
    public void testRESTClientFallbackTimeout() throws Exception {
       ShadowOneSignalRestClientWithMockConnection.mockResponse = new MockHttpURLConnection.MockResponse() {{
          mockThreadHang = true;
       }};
 
-      OneSignalPackagePrivateHelper.OneSignalRestClientPublic_getSync("URL", null);
+      OneSignalRestClient.getSync("URL", null);
       TestHelpers.threadAndTaskWait();
 
       assertTrue(ShadowOneSignalRestClientWithMockConnection.lastConnection.getDidInterruptMockHang());
+   }
+
+   private static final String SDK_VERSION_HTTP_HEADER = "onesignal/android/" + OneSignal.VERSION;
+
+   @Test
+   public void SDKHeaderIsIncludedInGetCalls() throws Exception {
+      OneSignalRestClient.getSync("URL", null);
+      TestHelpers.threadAndTaskWait();
+
+      assertEquals(SDK_VERSION_HTTP_HEADER, getLastHTTPHeaderProp("SDK-Version"));
+   }
+
+   @Test
+   public void SDKHeaderIsIncludedInPostCalls() throws Exception {
+      OneSignalRestClient.postSync("URL", null,null);
+      TestHelpers.threadAndTaskWait();
+
+      assertEquals(SDK_VERSION_HTTP_HEADER, getLastHTTPHeaderProp("SDK-Version"));
+   }
+
+   @Test
+   public void SDKHeaderIsIncludedInPutCalls() throws Exception {
+      OneSignalRestClient.putSync("URL", null,null);
+      TestHelpers.threadAndTaskWait();
+
+      assertEquals(SDK_VERSION_HTTP_HEADER, getLastHTTPHeaderProp("SDK-Version"));
+   }
+
+   private static String getLastHTTPHeaderProp(String prop) {
+      return ShadowOneSignalRestClientWithMockConnection.lastConnection.getRequestProperty(prop);
    }
 }
