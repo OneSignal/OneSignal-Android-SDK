018c5bffad05f99c15fc9e33b4357f2078a6102e
Josh Kasten <josh@onesignal.com>
2019-06-25 15:54:16 -0700

    Updated version to 031100
    * IAM is featured flagged on the backend with this version check.

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -384,7 +384,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   public static final String VERSION = "031009";
+   public static final String VERSION = "031100";
 
    private static AdvertisingIdentifierProvider mainAdIdProvider = new AdvertisingIdProviderGPS();
 
