3f8dabab528516148c45849ca4e967bbd3d1c8e9
Rodrigo <rogomezpalacio@protonmail.com>
2019-09-25 16:28:01 -0700

    Removed JobIntentService import causing custom class of same name to go unused

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/GcmIntentJobService.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/GcmIntentJobService.java
@@ -5,7 +5,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.JobIntentService;
+
+// Uses modified JobIntentService class that's part of the onesignal package
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class GcmIntentJobService extends JobIntentService {
