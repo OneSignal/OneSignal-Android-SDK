a28e1c22b6dfe39050a49d803c8d164fd85dc747
Michael DiCioccio <mdicioccio25@gmail.com>
2020-01-28 17:21:35 -0800

    Added a `try-catch` to the getDecorView line in `setCurActivity` (#933)
    * In a unity issue (#239) this was reported with no chance of reproducing it

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/ActivityLifecycleHandler.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/ActivityLifecycleHandler.java
@@ -92,11 +92,18 @@ class ActivityLifecycleHandler {
             entry.getValue().available(curActivity);
         }
 
-        ViewTreeObserver treeObserver = curActivity.getWindow().getDecorView().getViewTreeObserver();
-        for (Map.Entry<String, OSSystemConditionController.OSSystemConditionObserver> entry : sSystemConditionObservers.entrySet()) {
-            KeyboardListener keyboardListener = new KeyboardListener(entry.getValue(), entry.getKey());
-            treeObserver.addOnGlobalLayoutListener(keyboardListener);
-            sKeyboardListeners.put(entry.getKey(), keyboardListener);
+        try {
+            ViewTreeObserver treeObserver = curActivity.getWindow().getDecorView().getViewTreeObserver();
+            for (Map.Entry<String, OSSystemConditionController.OSSystemConditionObserver> entry : sSystemConditionObservers.entrySet()) {
+                KeyboardListener keyboardListener = new KeyboardListener(entry.getValue(), entry.getKey());
+                treeObserver.addOnGlobalLayoutListener(keyboardListener);
+                sKeyboardListeners.put(entry.getKey(), keyboardListener);
+            }
+        } catch (RuntimeException e) {
+            // Related to Unity Issue #239 on Github
+            // https://github.com/OneSignal/OneSignal-Unity-SDK/issues/239
+            // RuntimeException at ActivityLifecycleHandler.setCurActivity on Android (Unity 2.9.0)
+            e.printStackTrace();
         }
     }
 
