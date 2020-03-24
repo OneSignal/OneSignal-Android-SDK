da19b1b4765d1e499ea0612c7b96d13b6ec5a15c
mikechoch <mdicioccio25@gmail.com>
2020-03-23 19:48:04 -0700

    Release commit for `3.13.0`

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -424,7 +424,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   public static final String VERSION = "031207";
+   public static final String VERSION = "031300";
 
    private static OSSessionManager.SessionListener getNewSessionListener() {
       return new OSSessionManager.SessionListener() {
