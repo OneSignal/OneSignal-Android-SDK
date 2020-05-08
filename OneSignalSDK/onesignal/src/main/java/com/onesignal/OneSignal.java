fc63f392f09099f78d6b5a231c6c165eafefee7b
mikechoch <mdicioccio25@gmail.com>
2020-05-08 13:17:46 -0400

    Refactor prep for `InFocusDisplayOption` in `NotifForegroundHandler` * `NotificationWillShowInForegroundHandler` will be responsible for setting the notifications display type if a dev decides so   * Alternative is a global setter that also controls the default for `OSNotificationDisplay` if no `NotificationWillShowInForegroundHandler` is set   * Renamed `None` to `SILENT` and `Notification` to `NOTIFICATION` to follow a caps snake-case enum convention * Removed `InAppAlert` from `InFocusDisplayOption`   * Refactored `InFocusDisplayOption` to `OSNotificationDisplay`   * Cleaned out `InAppAlert` related `UnitTests`

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -2084,10 +2084,6 @@ public class OneSignal {
       runNotificationOpenedCallback(data, true);
    }
 
-   static OneSignalDbHelper getDBHelperInstance() {
-      return OneSignalDbHelper.getInstance(appContext);
-   }
-
    static boolean startOrResumeApp(Context inContext) {
       Intent launchIntent = inContext.getPackageManager().getLaunchIntentForPackage(inContext.getPackageName());
       // Make sure we have a launcher intent.
