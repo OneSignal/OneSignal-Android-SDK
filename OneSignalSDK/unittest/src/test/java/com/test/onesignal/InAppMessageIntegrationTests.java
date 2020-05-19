398a4511f6dabaf76e957b2a5f9b2ce297692afd
Jeasmine Nahui <jeas.nahui@gmail.com>
2020-05-18 23:17:23 -0300

    Add Huawei Location support

--- OneSignalSDK/unittest/src/test/java/com/test/onesignal/InAppMessageIntegrationTests.java
+++ OneSignalSDK/unittest/src/test/java/com/test/onesignal/InAppMessageIntegrationTests.java
@@ -19,6 +19,7 @@ import com.onesignal.ShadowCustomTabsClient;
 import com.onesignal.ShadowCustomTabsSession;
 import com.onesignal.ShadowDynamicTimer;
 import com.onesignal.ShadowJobService;
+import com.onesignal.ShadowLocationController;
 import com.onesignal.ShadowNotificationManagerCompat;
 import com.onesignal.ShadowOSUtils;
 import com.onesignal.ShadowOSViewUtils;
@@ -77,6 +78,7 @@ import static junit.framework.Assert.assertTrue;
                 ShadowOneSignalRestClient.class,
                 ShadowPushRegistratorGCM.class,
                 ShadowOSUtils.class,
+                ShadowLocationController.class,
                 ShadowAdvertisingIdProviderGPS.class,
                 ShadowCustomTabsClient.class,
                 ShadowCustomTabsSession.class,
