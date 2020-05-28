45e4b6337798a05e5b510c6f1d981a314a647266
Josh Kasten <josh@onesignal.com>
2020-05-27 21:37:27 -0700

    Added getDeviceType tests

--- OneSignalSDK/unittest/src/test/java/com/test/onesignal/MainOneSignalClassRunner.java
+++ OneSignalSDK/unittest/src/test/java/com/test/onesignal/MainOneSignalClassRunner.java
@@ -304,7 +304,7 @@ public class MainOneSignalClassRunner {
    @Test
    public void testDeviceTypeIsAmazon_forPlayerCreate() throws Exception {
       // 1. Mock Amazon device type for this test
-      ShadowOSUtils.mockAmazonDevice();
+      ShadowOSUtils.supportsADM = true;
 
       // 2. Init OneSignal so the app id is cached
       OneSignalInit();
@@ -344,7 +344,7 @@ public class MainOneSignalClassRunner {
    @Test
    public void testDeviceTypeIsAmazon_withoutOneSignalInit() throws Exception {
       // 1. Mock Amazon device type for this test
-      ShadowOSUtils.mockAmazonDevice();
+      ShadowOSUtils.supportsADM = true;
 
       // 2. Init OneSignal so the app id is cached
       OneSignalInit();
