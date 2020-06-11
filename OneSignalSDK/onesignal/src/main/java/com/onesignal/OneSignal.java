3b2504d7a4d7c0d4c69dcf51fd8db7c2461faecd
Josh Kasten <josh@onesignal.com>
2020-06-10 20:04:34 -0700

    Release commit 3.14.1

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -431,7 +431,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   public static final String VERSION = "031400";
+   public static final String VERSION = "031401";
 
    private static OSSessionManager.SessionListener sessionListener = new OSSessionManager.SessionListener() {
          @Override
