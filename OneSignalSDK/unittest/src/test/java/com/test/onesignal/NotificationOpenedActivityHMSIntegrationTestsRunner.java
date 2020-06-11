06fcb162912ba0b230ea4eeeeec75b78d2242d7f
Josh Kasten <josh@onesignal.com>
2020-06-11 00:33:27 -0700

    Moved logic out of NotificationOpenedActivityHMS
    * Moved most logic into NotificationPayloadProcessorHMS so the Activity
    has a single responbility, to be an entry point only.
    * Added HMS action button id formatting logic
    * Added new HMS open intergration test.
      - It covers testing firing the open callback + actionId is set.

--- OneSignalSDK/unittest/src/test/java/com/test/onesignal/NotificationOpenedActivityHMSIntegrationTestsRunner.java
+++ OneSignalSDK/unittest/src/test/java/com/test/onesignal/NotificationOpenedActivityHMSIntegrationTestsRunner.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import com.onesignal.NotificationOpenedActivityHMS;
+import com.onesignal.OSNotificationOpenResult;
 import com.onesignal.OneSignal;
 import com.onesignal.OneSignalPackagePrivateHelper.UserState;
 import com.onesignal.ShadowCustomTabsClient;
@@ -17,6 +18,7 @@ import com.onesignal.ShadowPushRegistratorHMS;
 import com.onesignal.StaticResetHelper;
 import com.onesignal.example.BlankActivity;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -30,13 +32,13 @@ import org.robolectric.shadows.ShadowLog;
 
 import java.util.UUID;
 
+import static com.onesignal.OneSignalPackagePrivateHelper.GenerateNotification.BUNDLE_KEY_ACTION_ID;
 import static com.onesignal.InAppMessagingHelpers.ONESIGNAL_APP_ID;
 import static com.test.onesignal.RestClientAsserts.assertNotificationOpenAtIndex;
 import static com.test.onesignal.TestHelpers.fastColdRestartApp;
 import static com.test.onesignal.TestHelpers.threadAndTaskWait;
 import static junit.framework.Assert.assertEquals;
 import static org.robolectric.Shadows.shadowOf;
-
 
 @Config(
     packageName = "com.onesignal.example",
@@ -68,27 +70,50 @@ public class NotificationOpenedActivityHMSIntegrationTestsRunner {
         ShadowOSUtils.supportsHMS(true);
     }
 
-    private static Intent helper_baseHMSOpenIntent() {
+    private static @NonNull Intent helper_baseHMSOpenIntent() {
         return new Intent()
                 .setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
                 .setAction("android.intent.action.VIEW");
+    }
+
+    private static @NonNull Intent helper_basicOSHMSOpenIntent() throws JSONException {
+        return helper_baseHMSOpenIntent()
+                .putExtra(
+                    "custom",
+                    new JSONObject() {{
+                        put("i", UUID.randomUUID().toString());
+                }}.toString()
+        );
+    }
+
+    private static @NonNull Intent helper_basicOSHMSOpenIntentWithActionId(final @NonNull String actionId) throws JSONException {
+        return helper_baseHMSOpenIntent()
+                .putExtra(
+                        "custom",
+                        new JSONObject() {{
+                            put("i", UUID.randomUUID().toString());
+                            put(BUNDLE_KEY_ACTION_ID, actionId);
+                        }}.toString()
+                );
     }
 
     private static void helper_startHMSOpenActivity(@NonNull Intent intent) {
         Robolectric.buildActivity(NotificationOpenedActivityHMS.class, intent).create();
     }
 
-    private static void helper_initSDKAndFireHMSNotificationOpenIntent() throws Exception {
+    private static void helper_initSDKAndFireHMSNotificationBarebonesOSOpenIntent() throws Exception {
+        Intent intent = helper_basicOSHMSOpenIntent();
+        helper_initSDKAndFireHMSNotificationOpenWithIntent(intent);
+    }
+
+    private static void helper_initSDKAndFireHMSNotificationActionButtonTapIntent(@NonNull String actionId) throws Exception {
+        Intent intent = helper_basicOSHMSOpenIntentWithActionId(actionId);
+        helper_initSDKAndFireHMSNotificationOpenWithIntent(intent);
+    }
+
+    private static void helper_initSDKAndFireHMSNotificationOpenWithIntent(@NonNull Intent intent) throws Exception {
         OneSignal.init(RuntimeEnvironment.application, "123456789", ONESIGNAL_APP_ID);
         fastColdRestartApp();
-
-        Intent intent = helper_baseHMSOpenIntent()
-                .putExtra(
-                        "custom",
-                        new JSONObject() {{
-                            put("i", UUID.randomUUID().toString());
-                        }}.toString()
-                );
 
         helper_startHMSOpenActivity(intent);
     }
@@ -102,7 +127,7 @@ public class NotificationOpenedActivityHMSIntegrationTestsRunner {
 
     @Test
     public void barebonesOSPayload_startsMainActivity() throws Exception {
-        helper_initSDKAndFireHMSNotificationOpenIntent();
+        helper_initSDKAndFireHMSNotificationBarebonesOSOpenIntent();
 
         Intent startedActivity = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
         assertEquals(startedActivity.getComponent().getClassName(), BlankActivity.class.getName());
@@ -110,8 +135,24 @@ public class NotificationOpenedActivityHMSIntegrationTestsRunner {
 
     @Test
     public void barebonesOSPayload_makesNotificationOpenRequest() throws Exception {
-        helper_initSDKAndFireHMSNotificationOpenIntent();
+        helper_initSDKAndFireHMSNotificationBarebonesOSOpenIntent();
         assertNotificationOpenAtIndex(1, UserState.DEVICE_TYPE_HUAWEI);
+    }
+
+    private static final String TEST_ACTION_ID = "myTestActionId";
+    private static String lastActionId;
+    @Test
+    public void firesOSNotificationOpenCallbackWithActionId() throws Exception {
+        helper_initSDKAndFireHMSNotificationActionButtonTapIntent(TEST_ACTION_ID);
+
+        OneSignal.startInit(RuntimeEnvironment.application).setNotificationOpenedHandler(new OneSignal.NotificationOpenedHandler() {
+            @Override
+            public void notificationOpened(OSNotificationOpenResult result) {
+                lastActionId = result.action.actionID;
+            }
+        }).init();
+
+        assertEquals(TEST_ACTION_ID, lastActionId);
     }
 
     @Test
