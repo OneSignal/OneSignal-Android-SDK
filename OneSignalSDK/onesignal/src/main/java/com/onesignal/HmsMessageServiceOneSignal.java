4da2f85a29bfcb50fd1d68942235644ea79e18e7
Josh Kasten <josh@onesignal.com>
2020-06-19 18:23:59 -0700

    HmsMessageService.onMessageReceived displaying
    * Now if an HMS "Data Message" is recieved in the OneSignal format it
    will be processed through the normal flow like GCM / FCM messages do.
       -  All features we support for FCM Android notificatons now apply to
             these types.

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/HmsMessageServiceOneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/HmsMessageServiceOneSignal.java
@@ -1,6 +1,7 @@
 package com.onesignal;
 
 import com.huawei.hms.push.HmsMessageService;
+import com.huawei.hms.push.RemoteMessage;
 
 public class HmsMessageServiceOneSignal extends HmsMessageService {
 
@@ -16,4 +17,17 @@ public class HmsMessageServiceOneSignal extends HmsMessageService {
         PushRegistratorHMS.fireCallback(token);
     }
 
+    /**
+     * This method is called in the following cases:
+     *   1. "Data messages" - App process is alive when received.
+     *   2. "Notification Message" - foreground_show = false and app is in focus
+     * This method callback must be completed in 10 seconds, start a new Job if more time is needed.
+     *   - This may only be a restriction for #1
+     *   - 10 sec limit didn't seem to hit for #2, even when backgrounding the app after the method starts
+     * @param message RemoteMessage
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        NotificationPayloadProcessorHMS.processDataMessageReceived(this, message.getData());
+    }
 }
