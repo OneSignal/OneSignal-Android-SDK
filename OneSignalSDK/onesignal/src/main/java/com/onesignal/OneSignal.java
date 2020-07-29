a1bd26130cac7cd83b25e63c5704cd9d505ce27c
mikechoch <mdicioccio25@gmail.com>
2020-07-29 10:19:31 -0400

    Native Android SDK release 3.15.2

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -431,7 +431,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   public static final String VERSION = "031501";
+   public static final String VERSION = "031502";
 
    private static OSSessionManager.SessionListener sessionListener = new OSSessionManager.SessionListener() {
          @Override
