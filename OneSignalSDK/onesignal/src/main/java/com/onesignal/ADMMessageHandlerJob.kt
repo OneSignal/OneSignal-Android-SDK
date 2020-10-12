7de8a1195f9c4838a6c6a79cdbded5804c3b5043
emawby <emawby@gmail.com>
2020-10-12 16:19:52 -0700

    creating ADMMessageHandlerJob class

---
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/ADMMessageHandlerJob.kt
@@ -0,0 +1,26 @@
+package com.onesignal
+
+import android.content.Context
+
+import android.content.Intent
+
+
+import com.amazon.device.messaging.ADMMessageHandlerJobBase
+
+class ADMMessageHandlerJob : ADMMessageHandlerJobBase() {
+    override fun onMessage(p0: Context?, p1: Intent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRegistered(p0: Context?, p1: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUnregistered(p0: Context?, p1: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRegistrationError(p0: Context?, p1: String?) {
+        TODO("Not yet implemented")
+    }
+}
