21727c1dd56f6a6c8ae256c1d75fa76f9533ac51
Josh Kasten <josh@onesignal.com>
2020-11-30 14:23:20 -0800

    WIP - attempting to switch a test to Spek
    * Attempted to convert a test to Spek2 style tests.
    * Spek2 requires JUnit5 which Robolectric does not support yet so Attempted to use Spek1.
       - Running into a rabbit hole of different issues with Spek1, it is quite old too so this isn't the path forward
    * Robolectric 5.0 will support JUnit 5 and Spek however
      - https://github.com/robolectric/robolectric/pull/5514
      - Also "Spek integration" PR started depending on Robolectric 5.0 Alpha branch https://github.com/robolectric/robolectric/pull/5520
    
    * Holding off integrating Spek until Robolectric 5.0 is closer to releasing.

--- OneSignalSDK/unittest/src/test/java/com/test/onesignal/DeviceTypeTestsRunner.kt
+++ OneSignalSDK/unittest/src/test/java/com/test/onesignal/DeviceTypeTestsRunner.kt
@@ -14,99 +14,129 @@ import org.robolectric.shadows.ShadowLog
 import com.onesignal.OneSignalPackagePrivateHelper.UserState.DEVICE_TYPE_ANDROID
 import com.onesignal.OneSignalPackagePrivateHelper.UserState.DEVICE_TYPE_FIREOS
 import com.onesignal.OneSignalPackagePrivateHelper.UserState.DEVICE_TYPE_HUAWEI
+import junit.framework.Assert.assertEquals
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
 
 @Config(packageName = "com.onesignal.example", shadows = [ShadowOSUtils::class], sdk = [26])
 @RunWith(RobolectricTestRunner::class)
-class DeviceTypeTestsRunner {
-    companion object {
-        @JvmStatic
-        @BeforeClass
-        fun setupClass() {
-            ShadowLog.stream = System.out
-            TestHelpers.beforeTestSuite()
-            StaticResetHelper.saveStaticValues()
-        }
-    }
-
-    @Before
-    @Throws(Exception::class)
-    fun beforeEachTest() {
+object DeviceTypeTestsRunner : Spek({
+    beforeEachTest {
         TestHelpers.beforeTestInitAndCleanup()
         OneSignal.initWithContext(ApplicationProvider.getApplicationContext())
     }
-
-    @Test
-    fun noAvailablePushChannels_defaultsToAndroid() {
-        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+    val deviceType =  OneSignalPackagePrivateHelper.getDeviceType()
+    it("defaults to Android DeviceType") {
+        Assert.assertEquals(deviceType, DEVICE_TYPE_ANDROID)
     }
 
-    @Test
-    fun onlyADM_isFireOS() {
-        ShadowOSUtils.supportsADM = true
-        Assert.assertEquals(DEVICE_TYPE_FIREOS.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+    describe("FireOS Device") {
+        beforeEachTest {
+            ShadowOSUtils.supportsADM = true
+        }
+
+        it("gives device_type FireOS") {
+            assertEquals(DEVICE_TYPE_FIREOS, OneSignalPackagePrivateHelper.getDeviceType())
+        }
     }
 
-    @Test
-    fun onlyFCM_isAndroid() {
-        ShadowOSUtils.hasFCMLibrary = true
-        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+    describe("Android Device with Play Services") {
+        beforeEachTest {
+            ShadowOSUtils.isGMSInstalledAndEnabled = true
+        }
+
+        it("gives device_type Android") {
+            assertEquals(DEVICE_TYPE_ANDROID, OneSignalPackagePrivateHelper.getDeviceType())
+        }
+
+        describe("gives device_type Android with FCM") {
+            beforeEachTest {
+                ShadowOSUtils.hasFCMLibrary = true
+            }
+
+            it("gives device_type Android") {
+                assertEquals(DEVICE_TYPE_ANDROID, OneSignalPackagePrivateHelper.getDeviceType())
+            }
+        }
     }
 
-    @Test
-    fun FCMAndGMSEnabled_isAndroid() {
-        ShadowOSUtils.isGMSInstalledAndEnabled = true
-        ShadowOSUtils.hasFCMLibrary = true
-        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+    describe("Huawei Device") {
+
     }
 
-    @Test
-    fun supportedHMS_isHuawei() {
-        ShadowOSUtils.isHMSCoreInstalledAndEnabled = true
-        ShadowOSUtils.hasAllRecommendedHMSLibraries(true)
-        Assert.assertEquals(DEVICE_TYPE_HUAWEI.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
-    }
-
-    @Test
-    fun supportsFCMAndHMS_PreferAndroid() {
-        ShadowOSUtils.isGMSInstalledAndEnabled = true
-        ShadowOSUtils.hasFCMLibrary = true
-        ShadowOSUtils.isHMSCoreInstalledAndEnabled = true
-        ShadowOSUtils.hasAllRecommendedHMSLibraries(true)
-
-        // Prefer Google Services over Huawei if both available
-        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
-    }
-
-    @Test
-    fun hasFCMButNoGMSOnDeviceAndHasHMS_isHuawei() {
-        ShadowOSUtils.isGMSInstalledAndEnabled = false
-        ShadowOSUtils.hasFCMLibrary = true
-        ShadowOSUtils.isHMSCoreInstalledAndEnabled = true
-        ShadowOSUtils.hasAllRecommendedHMSLibraries(true)
-
-        // Use HMS since device does not have the "Google Play services" app or it is disabled
-        Assert.assertEquals(DEVICE_TYPE_HUAWEI.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
-    }
-
-    @Test
-    fun noPushSDKsAndOnlyHMSCoreInstalled_isHuawei() {
-        ShadowOSUtils.isHMSCoreInstalledAndEnabled = true
-        Assert.assertEquals(DEVICE_TYPE_HUAWEI.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
-    }
-
-    @Test
-    fun noPushSDKsAndOnlyGoogleServicesInstalled_isAndroid() {
-        ShadowOSUtils.isGMSInstalledAndEnabled = true
-        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
-    }
-
-    @Test
-    fun supportsFCMAndADM_PreferADM() {
-        ShadowOSUtils.isGMSInstalledAndEnabled = true
-        ShadowOSUtils.hasFCMLibrary = true
-        ShadowOSUtils.supportsADM = true
-
-        // Prefer ADM as if available it will always be native to the device
-        Assert.assertEquals(DEVICE_TYPE_FIREOS.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
-    }
-}
+//    @Test
+//    fun noAvailablePushChannels_defaultsToAndroid() {
+//        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun onlyADM_isFireOS() {
+//        ShadowOSUtils.supportsADM = true
+//        Assert.assertEquals(DEVICE_TYPE_FIREOS.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun onlyFCM_isAndroid() {
+//        ShadowOSUtils.hasFCMLibrary = true
+//        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun FCMAndGMSEnabled_isAndroid() {
+//        ShadowOSUtils.isGMSInstalledAndEnabled = true
+//        ShadowOSUtils.hasFCMLibrary = true
+//        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun supportedHMS_isHuawei() {
+//        ShadowOSUtils.isHMSCoreInstalledAndEnabled = true
+//        ShadowOSUtils.hasAllRecommendedHMSLibraries(true)
+//        Assert.assertEquals(DEVICE_TYPE_HUAWEI.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun supportsFCMAndHMS_PreferAndroid() {
+//        ShadowOSUtils.isGMSInstalledAndEnabled = true
+//        ShadowOSUtils.hasFCMLibrary = true
+//        ShadowOSUtils.isHMSCoreInstalledAndEnabled = true
+//        ShadowOSUtils.hasAllRecommendedHMSLibraries(true)
+//
+//        // Prefer Google Services over Huawei if both available
+//        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun hasFCMButNoGMSOnDeviceAndHasHMS_isHuawei() {
+//        ShadowOSUtils.isGMSInstalledAndEnabled = false
+//        ShadowOSUtils.hasFCMLibrary = true
+//        ShadowOSUtils.isHMSCoreInstalledAndEnabled = true
+//        ShadowOSUtils.hasAllRecommendedHMSLibraries(true)
+//
+//        // Use HMS since device does not have the "Google Play services" app or it is disabled
+//        Assert.assertEquals(DEVICE_TYPE_HUAWEI.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun noPushSDKsAndOnlyHMSCoreInstalled_isHuawei() {
+//        ShadowOSUtils.isHMSCoreInstalledAndEnabled = true
+//        Assert.assertEquals(DEVICE_TYPE_HUAWEI.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun noPushSDKsAndOnlyGoogleServicesInstalled_isAndroid() {
+//        ShadowOSUtils.isGMSInstalledAndEnabled = true
+//        Assert.assertEquals(DEVICE_TYPE_ANDROID.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+//
+//    @Test
+//    fun supportsFCMAndADM_PreferADM() {
+//        ShadowOSUtils.isGMSInstalledAndEnabled = true
+//        ShadowOSUtils.hasFCMLibrary = true
+//        ShadowOSUtils.supportsADM = true
+//
+//        // Prefer ADM as if available it will always be native to the device
+//        Assert.assertEquals(DEVICE_TYPE_FIREOS.toLong(), OneSignalPackagePrivateHelper.getDeviceType().toLong())
+//    }
+})
