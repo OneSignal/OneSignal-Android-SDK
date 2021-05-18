74504bf728cc00fefcd9a90d67d856942249c663
Tanay Nigam <tanaynigam12@gmail.com>
2021-05-18 12:10:54 -0700

    Moved getCorrectedLanguage from OSUtils to LanguageProviderDevice
    * Moved the implementation of OSUtils.getCorrectedLanguage to LanguageProviderDevice.getLanguage

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/language/LanguageProviderDevice.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/language/LanguageProviderDevice.java
@@ -1,9 +1,23 @@
 package com.onesignal.language;
 
-import com.onesignal.OSUtils;
+import java.util.Locale;
 
 public class LanguageProviderDevice implements LanguageProvider{
     public String getLanguage() {
-        return OSUtils.getCorrectedLanguage();
+        String lang = Locale.getDefault().getLanguage();
+
+        // https://github.com/OneSignal/OneSignal-Android-SDK/issues/64
+        if (lang.equals("iw"))
+            return "he";
+        if (lang.equals("in"))
+            return "id";
+        if (lang.equals("ji"))
+            return "yi";
+
+        // https://github.com/OneSignal/OneSignal-Android-SDK/issues/98
+        if (lang.equals("zh"))
+            return lang + "-" + Locale.getDefault().getCountry();
+
+        return lang;
     }
 }
