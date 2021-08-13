672138f716f9579bf2a60b159e6305dcb52c7a14
Nan <nanftw@gmail.com>
2021-08-13 14:41:15 -0700

    Release commit 4.5.0

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -402,7 +402,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   private static final String VERSION = "040402";
+   private static final String VERSION = "040500";
    public static String getSdkVersionRaw() {
       return VERSION;
    }
