1c29e604759e04b9fb3c0687b7c58384e320917d
Nan <nanftw@gmail.com>
2021-08-26 00:58:27 -0700

    Check and pop `EXTERNAL_USER_ID_AUTH_HASH` every time we `persistState`
    - Every time we persistState, we check if the syncValues has an EXTERNAL_USER_ID_AUTH_HASH that is a candidate to be popped.
    - It is removed if either the EXTERNAL_USER_ID is "" (empty string) or not in the syncValues.
    - In some cases, when we set the external user ID to "", the EXTERNAL_USER_ID_AUTH_HASH is in the syncValues while the EXTERNAL_USER_ID is not but we want to pop the EXTERNAL_USER_ID_AUTH_HASH

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/UserState.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/UserState.java
@@ -15,6 +15,7 @@ import java.util.Set;
 
 import static com.onesignal.UserStateSynchronizer.APP_ID;
 import static com.onesignal.UserStateSynchronizer.EMAIL_AUTH_HASH_KEY;
+import static com.onesignal.UserStateSynchronizer.EXTERNAL_USER_ID;
 import static com.onesignal.UserStateSynchronizer.EXTERNAL_USER_ID_AUTH_HASH;
 import static com.onesignal.UserStateSynchronizer.SMS_AUTH_HASH_KEY;
 
@@ -318,6 +319,18 @@ abstract class UserState {
 
     void persistState() {
         synchronized(LOCK) {
+            // pop the EXTERNAL_USER_ID_AUTH_HASH if in process of removing external user ID
+            // external_user_id is either "" or not present
+            try {
+                if (syncValues.has(EXTERNAL_USER_ID_AUTH_HASH) &&
+                        ((syncValues.has(EXTERNAL_USER_ID) && syncValues.get(EXTERNAL_USER_ID).toString() == "") || !syncValues.has(EXTERNAL_USER_ID))) {
+                    syncValues.remove(EXTERNAL_USER_ID_AUTH_HASH);
+                    // the auth_hash is popped above but external user id may still remain as ""
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
             OneSignalPrefs.saveString(OneSignalPrefs.PREFS_ONESIGNAL,
                     OneSignalPrefs.PREFS_ONESIGNAL_USERSTATE_SYNCVALYES_ + persistKey, syncValues.toString());
             OneSignalPrefs.saveString(OneSignalPrefs.PREFS_ONESIGNAL,
