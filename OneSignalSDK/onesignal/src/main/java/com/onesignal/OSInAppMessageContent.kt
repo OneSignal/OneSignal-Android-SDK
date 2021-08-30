67b3f92c0502d3e0c4d62b1f67e3861b4800b7b0
emawby <emawby@gmail.com>
2021-08-30 14:55:34 -0700

    using height/width margin booleans instead of top/bottom/left/right double

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OSInAppMessageContent.kt
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OSInAppMessageContent.kt
@@ -5,17 +5,13 @@ import org.json.JSONObject
 const val HTML = "html"
 const val STYLES = "styles"
 const val DISPLAY_DURATION = "display_duration"
-const val TOP_MARGIN = "top_margin"
-const val BOTTOM_MARGIN = "bottom_margin"
-const val LEFT_MARGIN = "left_margin"
-const val RIGHT_MARGIN = "right_margin"
+const val REMOVE_HEIGHT_MARGIN = "remove_height_margin"
+const val REMOVE_WIDTH_MARGIN = "remove_width_margin"
 
 internal open class OSInAppMessageContent constructor(jsonObject: JSONObject) {
     var contentHtml: String? = null
-    var topMargin: Int? = 1
-    var bottomMargin: Int? = 1
-    var leftMargin: Int? = 1
-    var rightMargin: Int? = 1
+    var useHeightMargin: Boolean = true
+    var useWidthMargin: Boolean = true
     // The following properties are populated from Javascript events
     var displayLocation: WebViewManager.Position? = null
     var displayDuration: Double? = null
@@ -25,9 +21,7 @@ internal open class OSInAppMessageContent constructor(jsonObject: JSONObject) {
         contentHtml = jsonObject.optString(HTML)
         displayDuration = jsonObject.optDouble(DISPLAY_DURATION)
         var styles: JSONObject? = jsonObject.optJSONObject(STYLES)
-        topMargin = styles?.optInt(TOP_MARGIN, 1)
-        bottomMargin = styles?.optInt(BOTTOM_MARGIN, 1)
-        leftMargin = styles?.optInt(LEFT_MARGIN, 1)
-        rightMargin = styles?.optInt(RIGHT_MARGIN,1)
+        useHeightMargin = !(styles?.optBoolean(REMOVE_HEIGHT_MARGIN, false) ?: false)
+        useWidthMargin = !(styles?.optBoolean(REMOVE_WIDTH_MARGIN, false) ?: false)
     }
 }
