8404255d3ffcfc3cae7f50248386adc54016f0e7
emawby <emawby@gmail.com>
2021-10-18 14:29:47 -0700

    enable staging testing

--- Examples/OneSignalDemo/app/src/main/java/com/onesignal/sdktest/application/MainApplication.java
+++ Examples/OneSignalDemo/app/src/main/java/com/onesignal/sdktest/application/MainApplication.java
@@ -68,7 +68,7 @@ public class MainApplication extends MultiDexApplication {
         });
 
         OneSignal.unsubscribeWhenNotificationsAreDisabled(true);
-        OneSignal.pauseInAppMessages(true);
+        OneSignal.pauseInAppMessages(false);
         OneSignal.setLocationShared(false);
 
         Log.d(Tag.DEBUG, Text.ONESIGNAL_SDK_INIT);
