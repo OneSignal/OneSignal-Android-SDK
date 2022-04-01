6173b33916ca443cdfc982319596f5c606c43b73
Josh Kasten <josh@onesignal.com>
2022-04-01 14:16:01 -0700

    Pulled in latest OPPO impl from ShortcutBadger
    Pulled in latest source for just OPPOHomeBader.java to remove usage of
    Runtime.getRuntime().exec which is flagged as an inscure
    API by static analysis tools.
    
    Source used the following (package and imports modified to embed into this project):
    https://github.com/leolin310148/ShortcutBadger/blob/822ad57726c74b34ba16386ce66df10351aed9ae/ShortcutBadger/src/main/java/me/leolin/shortcutbadger/impl/OPPOHomeBader.java
    
    Required upstream changes where also brought into BroadcastHelper.java
    that the latest OPPOHomeBader.java depends on.

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/shortcutbadger/util/BroadcastHelper.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/shortcutbadger/util/BroadcastHelper.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 
+import com.onesignal.shortcutbadger.ShortcutBadgeException;
+
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,5 +18,29 @@ public class BroadcastHelper {
 		PackageManager packageManager = context.getPackageManager();
 		List<ResolveInfo> receivers = packageManager.queryBroadcastReceivers(intent, 0);
 		return receivers != null && receivers.size() > 0;
+	}
+
+	public static List<ResolveInfo> resolveBroadcast(Context context, Intent intent) {
+		PackageManager packageManager = context.getPackageManager();
+		List<ResolveInfo> receivers = packageManager.queryBroadcastReceivers(intent, 0);
+
+		return receivers != null ? receivers : Collections.<ResolveInfo>emptyList();
+	}
+
+	public static void sendIntentExplicitly(Context context, Intent intent) throws ShortcutBadgeException {
+		List<ResolveInfo> resolveInfos = resolveBroadcast(context, intent);
+
+		if (resolveInfos.size() == 0) {
+			throw new ShortcutBadgeException("unable to resolve intent: " + intent.toString());
+		}
+
+		for (ResolveInfo info : resolveInfos) {
+			Intent actualIntent = new Intent(intent);
+
+			if (info != null) {
+				actualIntent.setPackage(info.resolvePackageName);
+				context.sendBroadcast(actualIntent);
+			}
+		}
 	}
 }
