b07af23c80a2c715a1d6711558e526edf387b3c7
Josh Kasten <josh@onesignal.com>
2022-04-21 10:11:01 -0700

    Convert JUnit5 asserts to Kotest should

--- OneSignalSDK/onesignal/user/src/test/java/com/onesignal/user/UserIdentityTest.kt
+++ OneSignalSDK/onesignal/user/src/test/java/com/onesignal/user/UserIdentityTest.kt
@@ -1,15 +1,15 @@
 package com.onesignal.user
 
 import io.kotest.core.spec.style.DescribeSpec
-
-import kotlin.test.assertEquals
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldNotBeSameInstanceAs
 
 class UserIdentityTest : DescribeSpec({
     describe("An UserAnonymous") {
         val user = UserAnonymous()
         context("identity") {
-            it("is equal a different UserIdentity.Anonymous instance") {
-                assertEquals(UserIdentity.Anonymous(), user.identity)
+            it("is equal to a different UserIdentity.Anonymous instance") {
+                user.identity shouldNotBeSameInstanceAs UserIdentity.Anonymous()
             }
         }
     }
@@ -21,7 +21,7 @@ class UserIdentityTest : DescribeSpec({
             val getNewIdentity = { UserIdentity.ExternalIdWithoutAuth(myId) }
             it("is equal to ExternalIdWithoutAuth") {
                 val user = UserIdentified(getNewIdentity())
-                assertEquals(getNewIdentity(), user.identity)
+                user.identity shouldBe getNewIdentity()
             }
         }
 
@@ -30,7 +30,7 @@ class UserIdentityTest : DescribeSpec({
             val getNewIdentity = { UserIdentity.ExternalIdWithAuthHash(myId, mockHash) }
             it("is equal to ExternalIdWithAuthHash") {
                 val user = UserIdentified(getNewIdentity())
-                assertEquals(getNewIdentity(), user.identity)
+                user.identity shouldBe getNewIdentity()
             }
         }
     }
--- OneSignalSDK/onesignal/user/src/test/java/com/onesignal/user/UserManagerTest.kt
+++ OneSignalSDK/onesignal/user/src/test/java/com/onesignal/user/UserManagerTest.kt
@@ -1,10 +1,9 @@
 package com.onesignal.user
 
-import kotlin.test.assertNotSame
-import kotlin.test.assertSame
-import kotlin.test.assertTrue
-
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.kotest.matchers.types.shouldBeTypeOf
+import io.kotest.matchers.types.shouldNotBeSameInstanceAs
 
 class UserManagerTest : DescribeSpec({
     describe("default UserManager") {
@@ -13,28 +12,28 @@ class UserManagerTest : DescribeSpec({
 
         context("get user") {
             it("gives UserAnonymous") {
-                assertTrue(user is UserAnonymous)
+                user.shouldBeTypeOf<UserAnonymous>()
             }
 
             it("gives same UserAnonymous instance on 2nd call") {
-                assertSame(user, userManager.user)
+                userManager.user shouldBeSameInstanceAs user
             }
         }
 
         context("getUserBy") {
             it("gives same UserAnonymous - UserIdentity.Anonymous") {
-                assertSame(user, userManager.getUserBy(UserIdentity.Anonymous()))
+                userManager.getUserBy(UserIdentity.Anonymous()) shouldBeSameInstanceAs user
             }
 
             context("Identified") {
                 val mockId = "mockId"
+                val userWithId = userManager.getUserBy(UserIdentity.ExternalIdWithoutAuth(mockId))
                 it("gives UserIdentified with ExternalIdWithoutAuth") {
-                    val user = userManager.getUserBy(UserIdentity.ExternalIdWithoutAuth(mockId))
-                    assertTrue(user is UserIdentified)
+                    userWithId.shouldBeTypeOf<UserIdentified>()
                 }
 
                 it("gives differ User with ExternalIdWithoutAuth") {
-                    assertNotSame(user, userManager.getUserBy(UserIdentity.ExternalIdWithoutAuth(mockId)))
+                    userWithId shouldNotBeSameInstanceAs user
                 }
             }
         }
