3a6bcd4f7c36fa0a95bd037b9c9d64fef92d2002
Brian Smith <briansmith@onesignal.com>
2022-08-25 15:46:17 -0400

    [User Model] Migrate Purchase Tracking * Adjust IStartableService so it is called *after* the appId has been established. This is done to remove the extra step of having startable services register a listener to wait for the appId to be set. * Create RefreshParamsService as a startable service to pull the remote parameters from the backend. This was previously part of StartupService, separated as a refactor. * Migrate TrackAmazonPurchase and TrackGooglePurchase which are startable services that track amazon and google purchases (respectively).  When captured these get pushed to the operation repo via TrackPurchaseOperation.  The execution of that operation to-be-implemented.

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/onesignal/core/internal/startup/IStartableService.kt
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/onesignal/core/internal/startup/IStartableService.kt
@@ -1,23 +1,29 @@
 package com.onesignal.onesignal.core.internal.startup
 
-import androidx.annotation.WorkerThread
 import com.onesignal.onesignal.core.OneSignal
+import com.onesignal.onesignal.core.internal.application.IApplicationService
+import com.onesignal.onesignal.core.internal.models.ConfigModel
+import com.onesignal.onesignal.core.internal.models.ConfigModelStore
 
 /**
  * Implement and provide this interface as part of service registration to indicate the service
  * wants to be instantiated and its [start] function called during the initialization process.
- * When the SDK is initialized via [OneSignal.initWithContext], all "startable" services will be
- * processed.
  *
- * When started there is no guarantee that any data is available.  Typically a startable service
+ * When [IStartableService.start] is called, both [OneSignal.initWithContext] and [OneSignal.setAppId]
+ * have been called.  This means the following is true:
+ *
+ *  1) An appContext is available in [IApplicationService.appContext].
+ *  2) An appId is available in [ConfigModel.appId] via [ConfigModelStore.get]
+ *
+ * When started there is no guarantee that any other data is available.  Typically a startable service
  * must be instantiated immediately and will add their appropriate hooks to then respond to changes
  * in the system.
  */
 interface IStartableService {
 
     /**
-     * Called when the service is to be started.
+     * Called when the service is to be started.  The appId and appContext have already been
+     * established.
      */
-    @WorkerThread
     fun start()
 }
