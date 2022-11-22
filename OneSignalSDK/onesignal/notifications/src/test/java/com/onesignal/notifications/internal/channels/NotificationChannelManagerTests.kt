1ebd737b8ac26c7b075d7504e68e950e22acc2db
Brian Smith <briansmith@onesignal.com>
2022-11-22 15:48:09 -0500

    [User Model] Notifications Unit Tests * Added various unit tests for notifications module * removed high level unittest module from solution

---
+++ OneSignalSDK/onesignal/notifications/src/test/java/com/onesignal/notifications/internal/channels/NotificationChannelManagerTests.kt
@@ -0,0 +1,292 @@
+package com.onesignal.notifications.internal.channels
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.onesignal.debug.LogLevel
+import com.onesignal.debug.internal.logging.Logging
+import com.onesignal.mocks.AndroidMockHelper
+import com.onesignal.mocks.MockHelper
+import com.onesignal.notifications.extensions.RobolectricTest
+import com.onesignal.notifications.internal.channels.impl.NotificationChannelManager
+import com.onesignal.notifications.internal.common.NotificationGenerationJob
+import com.onesignal.notifications.shadows.ShadowRoboNotificationManager
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import io.kotest.runner.junit4.KotestTestRunner
+import org.json.JSONArray
+import org.json.JSONObject
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import java.math.BigInteger
+
+@Config(
+    packageName = "com.onesignal.example",
+    shadows = [ShadowRoboNotificationManager::class],
+    sdk = [26]
+)
+@RobolectricTest
+@RunWith(KotestTestRunner::class)
+class NotificationChannelManagerTests : FunSpec({
+    beforeAny {
+        Logging.logLevel = LogLevel.NONE
+    }
+
+    test("createNotificationChannel should return default channel with empty payload") {
+        /* Given */
+        val mockTime = MockHelper.time(1111)
+
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+
+        /* When */
+        val response = notificationChannelManager.createNotificationChannel(NotificationGenerationJob(JSONObject(), mockTime))
+
+        /* Then */
+        response shouldBe "fcm_fallback_notification_channel"
+
+        val lastChannel = ShadowRoboNotificationManager.lastChannel
+        lastChannel shouldNotBe null
+        lastChannel!!.id shouldBe "fcm_fallback_notification_channel"
+        lastChannel.sound shouldNotBe null
+        lastChannel.shouldShowLights() shouldBe true
+        lastChannel.shouldVibrate() shouldBe true
+    }
+
+    test("createNotificationChannel should create basic channel") {
+        /* Given */
+        val mockTime = MockHelper.time(1111)
+
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+        val payload = JSONObject()
+            .put(
+                "chnl",
+                JSONObject()
+                    .put("id", "test_id")
+            )
+
+        /* When */
+        val response = notificationChannelManager.createNotificationChannel(NotificationGenerationJob(payload, mockTime))
+
+        /* Then */
+        response shouldBe "test_id"
+
+        val lastChannel = ShadowRoboNotificationManager.lastChannel
+        lastChannel shouldNotBe null
+        lastChannel!!.id shouldBe "test_id"
+        lastChannel.sound shouldNotBe null
+        lastChannel.shouldShowLights() shouldBe true
+        lastChannel.shouldVibrate() shouldBe true
+    }
+
+    test("createNotificationChannel with all options") {
+        /* Given */
+        val mockTime = MockHelper.time(1111)
+
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+        val payload = JSONObject()
+            .put("pri", 10)
+            .put("led", 0)
+            .put("ledc", "FFFF0000")
+            .put("vib", 0)
+            .put("vib_pt", JSONArray("[1,2,3,4]"))
+            .put("sound", "notification")
+            .put("vis", Notification.VISIBILITY_SECRET)
+            .put("bdg", 1)
+            .put("bdnd", 1)
+            .put(
+                "chnl",
+                JSONObject()
+                    .put("id", "test_id")
+                    .put("nm", "Test Name")
+                    .put("dscr", "Some description")
+                    .put("grp_id", "grp_id")
+                    .put("grp_nm", "Group Name")
+            )
+
+        /* When */
+        val response = notificationChannelManager.createNotificationChannel(NotificationGenerationJob(payload, mockTime))
+
+        /* Then */
+        response shouldBe "test_id"
+
+        val lastChannel = ShadowRoboNotificationManager.lastChannel
+        val lastGroup = ShadowRoboNotificationManager.lastChannelGroup
+
+        lastChannel shouldNotBe null
+        lastChannel!!.id shouldBe "test_id"
+        lastChannel.name shouldBe "Test Name"
+        lastChannel.description shouldBe "Some description"
+        lastChannel.group shouldBe "grp_id"
+
+        lastGroup shouldNotBe null
+        lastGroup!!.id shouldBe "grp_id"
+        lastGroup.name shouldBe "Group Name"
+
+        lastChannel.sound shouldNotBe null
+        lastChannel.shouldShowLights() shouldBe false // Setting a led color should NOT override enableLights
+
+        lastChannel.lightColor.toLong() shouldBe -65536
+        lastChannel.shouldVibrate() shouldBe false // Setting a pattern should NOT override enableVibration
+
+        lastChannel.vibrationPattern shouldBe longArrayOf(1, 2, 3, 4)
+
+        lastChannel.importance.toLong() shouldBe NotificationManager.IMPORTANCE_MAX
+        lastChannel.sound.toString() shouldBe "content://settings/system/notification_sound"
+        lastChannel.lockscreenVisibility.toLong() shouldBe Notification.VISIBILITY_SECRET.toLong()
+        lastChannel.canShowBadge() shouldBe true
+        lastChannel.canBypassDnd() shouldBe true
+    }
+
+    test("createNotificationChannel use other channel when available") {
+        /* Given */
+        val mockTime = MockHelper.time(1111)
+
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+        val payload = JSONObject()
+            .put("oth_chnl", "existing_id")
+            .put("chnl", JSONObject().put("id", "test_id"))
+
+        /* When */
+        val response1 = notificationChannelManager.createNotificationChannel(NotificationGenerationJob(payload, mockTime))
+        createChannel("existing_id", ApplicationProvider.getApplicationContext())
+        val response2 = notificationChannelManager.createNotificationChannel(NotificationGenerationJob(payload, mockTime))
+
+        /* Then */
+        response1 shouldBe "test_id"
+        response2 shouldBe "existing_id"
+    }
+
+    test("createNotificationChannel with invalid color should revert to FFFFFFFF") {
+        /* Given */
+        val mockTime = MockHelper.time(1111)
+
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+        val payload = JSONObject()
+            .put("ledc", "FFFFFFFFY")
+            .put(
+                "chnl",
+                JSONObject()
+                    .put("id", "test_id")
+                    .put("nm", "Test Name")
+            )
+
+        /* When */
+        val response = notificationChannelManager.createNotificationChannel(NotificationGenerationJob(payload, mockTime))
+
+        /* Then */
+        val lastChannel = ShadowRoboNotificationManager.lastChannel
+        response shouldBe "test_id"
+        lastChannel shouldNotBe null
+        lastChannel!!.lightColor shouldBe BigInteger("FFFFFFFF", 16).toInt()
+    }
+
+    test("processChannelList with no channel list should keep existing channels") {
+        /* Given */
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+
+        createChannel("local_existing_id", ApplicationProvider.getApplicationContext())
+        createChannel("OS_existing_id", ApplicationProvider.getApplicationContext())
+
+        /* When */
+        notificationChannelManager.processChannelList(null)
+
+        /* Then */
+        getChannel("local_existing_id", ApplicationProvider.getApplicationContext()) shouldNotBe null
+        getChannel("OS_existing_id", ApplicationProvider.getApplicationContext()) shouldNotBe null
+    }
+
+    test("processChannelList with existing local channel should not delete local channel") {
+        /* Given */
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+
+        createChannel("local_existing_id", ApplicationProvider.getApplicationContext())
+
+        val payload = JSONArray()
+            .put(
+                JSONObject()
+                    .put("chnl", JSONObject().put("id", "OS_id1"))
+            )
+
+        /* When */
+        notificationChannelManager.processChannelList(payload)
+
+        /* Then */
+        getChannel("local_existing_id", ApplicationProvider.getApplicationContext()) shouldNotBe null
+        getChannel("OS_id1", ApplicationProvider.getApplicationContext()) shouldNotBe null
+    }
+
+    test("processChannelList with existing OS channel should delete old OS channel when it is not in channel list") {
+        /* Given */
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+
+        createChannel("local_existing_id", ApplicationProvider.getApplicationContext())
+        createChannel("OS_existing_id", ApplicationProvider.getApplicationContext())
+
+        val payload = JSONArray()
+            .put(
+                JSONObject()
+                    .put("chnl", JSONObject().put("id", "OS_id1"))
+            )
+
+        /* When */
+        notificationChannelManager.processChannelList(payload)
+
+        /* Then */
+        getChannel("local_existing_id", ApplicationProvider.getApplicationContext()) shouldNotBe null
+        getChannel("OS_existing_id", ApplicationProvider.getApplicationContext()) shouldBe null
+        getChannel("OS_id1", ApplicationProvider.getApplicationContext()) shouldNotBe null
+    }
+
+    test("processChannelList multilanguage") {
+        /* Given */
+        val notificationChannelManager = NotificationChannelManager(AndroidMockHelper.applicationService(), MockHelper.languageContext())
+
+        val payload = JSONArray()
+            .put(
+                JSONObject()
+                    .put(
+                        "chnl",
+                        JSONObject()
+                            .put("id", "OS_id1")
+                            .put("grp_id", "grp_id1")
+                            .put(
+                                "langs",
+                                JSONObject()
+                                    .put(
+                                        "en",
+                                        JSONObject()
+                                            .put("nm", "en_nm")
+                                            .put("dscr", "en_dscr")
+                                            .put("grp_nm", "en_grp_nm")
+                                    )
+                            )
+                    )
+            )
+
+        /* When */
+        notificationChannelManager.processChannelList(payload)
+
+        /* Then */
+        val lastGroup = ShadowRoboNotificationManager.lastChannelGroup
+        val channel = getChannel("OS_id1", ApplicationProvider.getApplicationContext())
+        channel shouldNotBe null
+        channel!!.name shouldBe "en_nm"
+        channel.description shouldBe "en_dscr"
+        lastGroup shouldNotBe null
+        lastGroup!!.name shouldBe "en_grp_nm"
+    }
+})
+
+fun createChannel(id: String, context: Context) {
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    val channel = NotificationChannel(id, "name", NotificationManager.IMPORTANCE_DEFAULT)
+    notificationManager.createNotificationChannel(channel)
+}
+
+private fun getChannel(id: String, context: Context): NotificationChannel? {
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    return notificationManager.getNotificationChannel(id)
+}
---
+++ OneSignalSDK/onesignal/notifications/src/test/java/com/onesignal/notifications/internal/pushtoken/PushTokenManagerTests.kt
@@ -0,0 +1,195 @@
+package com.onesignal.notifications.internal.pushtoken
+
+import com.onesignal.core.internal.device.IDeviceService
+import com.onesignal.debug.LogLevel
+import com.onesignal.debug.internal.logging.Logging
+import com.onesignal.mocks.MockHelper
+import com.onesignal.notifications.internal.registration.IPushRegistrator
+import com.onesignal.notifications.shadows.ShadowRoboNotificationManager
+import com.onesignal.user.internal.subscriptions.SubscriptionStatus
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.runner.junit4.KotestTestRunner
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.runner.RunWith
+
+@RunWith(KotestTestRunner::class)
+class PushTokenManagerTests : FunSpec({
+    beforeAny {
+        Logging.logLevel = LogLevel.NONE
+        ShadowRoboNotificationManager.reset()
+    }
+
+    test("retrievePushToken should fail with missing library when android support libraries are missing") {
+        /* Given */
+        val mockPushRegistrator = mockk<IPushRegistrator>()
+        val mockDeviceService = MockHelper.deviceService()
+        every { mockDeviceService.androidSupportLibraryStatus } returns IDeviceService.AndroidSupportLibraryStatus.MISSING
+
+        val pushTokenManager = PushTokenManager(mockPushRegistrator, mockDeviceService)
+
+        /* When */
+        val response = pushTokenManager.retrievePushToken()
+        val pushToken = pushTokenManager.pushToken
+        val pushTokenStatus = pushTokenManager.pushTokenStatus
+
+        /* Then */
+        response.token shouldBe null
+        response.status shouldBe SubscriptionStatus.MISSING_ANDROID_SUPPORT_LIBRARY
+        pushToken shouldBe null
+        pushTokenStatus shouldBe SubscriptionStatus.MISSING_ANDROID_SUPPORT_LIBRARY
+    }
+
+    test("retrievePushToken should fail with outdated library when android support libraries are missing") {
+        /* Given */
+        val mockPushRegistrator = mockk<IPushRegistrator>()
+        val mockDeviceService = MockHelper.deviceService()
+        every { mockDeviceService.androidSupportLibraryStatus } returns IDeviceService.AndroidSupportLibraryStatus.OUTDATED
+
+        val pushTokenManager = PushTokenManager(mockPushRegistrator, mockDeviceService)
+
+        /* When */
+        val response = pushTokenManager.retrievePushToken()
+        val pushToken = pushTokenManager.pushToken
+        val pushTokenStatus = pushTokenManager.pushTokenStatus
+
+        /* Then */
+        response.token shouldBe null
+        response.status shouldBe SubscriptionStatus.OUTDATED_ANDROID_SUPPORT_LIBRARY
+        pushToken shouldBe null
+        pushTokenStatus shouldBe SubscriptionStatus.OUTDATED_ANDROID_SUPPORT_LIBRARY
+    }
+
+    test("retrievePushToken should succeed when registration is successful") {
+        /* Given */
+        val mockPushRegistrator = mockk<IPushRegistrator>()
+        coEvery { mockPushRegistrator.registerForPush() } returns IPushRegistrator.RegisterResult("pushToken", SubscriptionStatus.SUBSCRIBED)
+        val mockDeviceService = MockHelper.deviceService()
+        every { mockDeviceService.androidSupportLibraryStatus } returns IDeviceService.AndroidSupportLibraryStatus.OK
+
+        val pushTokenManager = PushTokenManager(mockPushRegistrator, mockDeviceService)
+
+        /* When */
+        val response = pushTokenManager.retrievePushToken()
+        val pushToken = pushTokenManager.pushToken
+        val pushTokenStatus = pushTokenManager.pushTokenStatus
+
+        /* Then */
+        coVerify(exactly = 1) { mockPushRegistrator.registerForPush() }
+        response.token shouldBe "pushToken"
+        response.status shouldBe SubscriptionStatus.SUBSCRIBED
+        pushToken shouldBe "pushToken"
+        pushTokenStatus shouldBe SubscriptionStatus.SUBSCRIBED
+    }
+
+    test("retrievePushToken should fail with failure status from push registrator with config-type error") {
+        /* Given */
+        val mockPushRegistrator = mockk<IPushRegistrator>()
+        coEvery { mockPushRegistrator.registerForPush() } returns IPushRegistrator.RegisterResult(null, SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY)
+        val mockDeviceService = MockHelper.deviceService()
+        every { mockDeviceService.androidSupportLibraryStatus } returns IDeviceService.AndroidSupportLibraryStatus.OK
+
+        val pushTokenManager = PushTokenManager(mockPushRegistrator, mockDeviceService)
+
+        /* When */
+        val response = pushTokenManager.retrievePushToken()
+        val pushToken = pushTokenManager.pushToken
+        val pushTokenStatus = pushTokenManager.pushTokenStatus
+
+        /* Then */
+        coVerify(exactly = 1) { mockPushRegistrator.registerForPush() }
+        response.token shouldBe null
+        response.status shouldBe SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY
+        pushToken shouldBe null
+        pushTokenStatus shouldBe SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY
+    }
+
+    test("retrievePushToken should fail with failure status from push registrator with runtime-type error") {
+        /* Given */
+        val mockPushRegistrator = mockk<IPushRegistrator>()
+        coEvery { mockPushRegistrator.registerForPush() } returns IPushRegistrator.RegisterResult(null, SubscriptionStatus.FIREBASE_FCM_ERROR_MISC_EXCEPTION)
+        val mockDeviceService = MockHelper.deviceService()
+        every { mockDeviceService.androidSupportLibraryStatus } returns IDeviceService.AndroidSupportLibraryStatus.OK
+
+        val pushTokenManager = PushTokenManager(mockPushRegistrator, mockDeviceService)
+
+        /* When */
+        val response = pushTokenManager.retrievePushToken()
+        val pushToken = pushTokenManager.pushToken
+        val pushTokenStatus = pushTokenManager.pushTokenStatus
+
+        /* Then */
+        coVerify(exactly = 1) { mockPushRegistrator.registerForPush() }
+        response.token shouldBe null
+        response.status shouldBe SubscriptionStatus.FIREBASE_FCM_ERROR_MISC_EXCEPTION
+        pushToken shouldBe null
+        pushTokenStatus shouldBe SubscriptionStatus.FIREBASE_FCM_ERROR_MISC_EXCEPTION
+    }
+
+    test("retrievePushToken should fail with failure status of config-type error even if subsequent runtime-type error") {
+        /* Given */
+        val mockPushRegistrator = mockk<IPushRegistrator>()
+        coEvery { mockPushRegistrator.registerForPush() } returns
+            IPushRegistrator.RegisterResult(null, SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY) andThen
+            IPushRegistrator.RegisterResult(null, SubscriptionStatus.FIREBASE_FCM_ERROR_MISC_EXCEPTION)
+
+        val mockDeviceService = MockHelper.deviceService()
+        every { mockDeviceService.androidSupportLibraryStatus } returns IDeviceService.AndroidSupportLibraryStatus.OK
+
+        val pushTokenManager = PushTokenManager(mockPushRegistrator, mockDeviceService)
+
+        /* When */
+        val response1 = pushTokenManager.retrievePushToken()
+        val pushToken1 = pushTokenManager.pushToken
+        val pushTokenStatus1 = pushTokenManager.pushTokenStatus
+        val response2 = pushTokenManager.retrievePushToken()
+        val pushToken2 = pushTokenManager.pushToken
+        val pushTokenStatus2 = pushTokenManager.pushTokenStatus
+
+        /* Then */
+        coVerify(exactly = 2) { mockPushRegistrator.registerForPush() }
+        response1.token shouldBe null
+        response1.status shouldBe SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY
+        pushToken1 shouldBe null
+        pushTokenStatus1 shouldBe SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY
+        response2.token shouldBe null
+        response2.status shouldBe SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY
+        pushToken2 shouldBe null
+        pushTokenStatus2 shouldBe SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY
+    }
+
+    test("retrievePushToken should fail with failure status of config-type error even if previous runtime-type error") {
+        /* Given */
+        val mockPushRegistrator = mockk<IPushRegistrator>()
+        coEvery { mockPushRegistrator.registerForPush() } returns
+            IPushRegistrator.RegisterResult(null, SubscriptionStatus.FIREBASE_FCM_ERROR_MISC_EXCEPTION) andThen
+            IPushRegistrator.RegisterResult(null, SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY)
+
+        val mockDeviceService = MockHelper.deviceService()
+        every { mockDeviceService.androidSupportLibraryStatus } returns IDeviceService.AndroidSupportLibraryStatus.OK
+
+        val pushTokenManager = PushTokenManager(mockPushRegistrator, mockDeviceService)
+
+        /* When */
+        val response1 = pushTokenManager.retrievePushToken()
+        val pushToken1 = pushTokenManager.pushToken
+        val pushTokenStatus1 = pushTokenManager.pushTokenStatus
+        val response2 = pushTokenManager.retrievePushToken()
+        val pushToken2 = pushTokenManager.pushToken
+        val pushTokenStatus2 = pushTokenManager.pushTokenStatus
+
+        /* Then */
+        coVerify(exactly = 2) { mockPushRegistrator.registerForPush() }
+        response1.token shouldBe null
+        response1.status shouldBe SubscriptionStatus.FIREBASE_FCM_ERROR_MISC_EXCEPTION
+        pushToken1 shouldBe null
+        pushTokenStatus1 shouldBe SubscriptionStatus.FIREBASE_FCM_ERROR_MISC_EXCEPTION
+        response2.token shouldBe null
+        response2.status shouldBe SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY
+        pushToken2 shouldBe null
+        pushTokenStatus2 shouldBe SubscriptionStatus.MISSING_FIREBASE_FCM_LIBRARY
+    }
+})
