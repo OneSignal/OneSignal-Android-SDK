7fa1ff4923a3f8b686118ca6236f5d0b34da4ea0
Brian Smith <briansmith@onesignal.com>
2022-12-07 10:07:00 -0500

    [User Model] Backend Integration * Implement `UserBackendService`, `IdentityBackendService`, and `SubscriptionBackendService` * Tweak `OutcomeEventsBackendService` to provide `subscriptionId`

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/session/internal/outcomes/impl/IOutcomeEventsBackendService.kt
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/session/internal/outcomes/impl/IOutcomeEventsBackendService.kt
@@ -2,6 +2,8 @@ package com.onesignal.session.internal.outcomes.impl
 
 import com.onesignal.common.exceptions.BackendException
 import com.onesignal.core.internal.device.IDeviceService
+import com.onesignal.user.internal.subscriptions.SubscriptionModel
+import com.onesignal.user.subscriptions.ISubscription
 
 /**
  * The backend service for outcomes.
@@ -14,9 +16,10 @@ internal interface IOutcomeEventsBackendService {
      * If there is a non-successful response from the backend, a [BackendException] will be thrown with response data.
      *
      * @param appId The ID of the application this outcome event occurred under.
-     * @param deviceType The device type.
+     * @param userId The OneSignal user ID that is active during the outcome event.
+     * @param subscriptionId The subscription ID that is active during the outcome event.
      * @param direct Whether this outcome event is direct. `true` if it is, `false` if it isn't, `null` if should not be specified.
      * @param event The outcome event to send up.
      */
-    suspend fun sendOutcomeEvent(appId: String, deviceType: IDeviceService.DeviceType, direct: Boolean?, event: OutcomeEvent)
+    suspend fun sendOutcomeEvent(appId: String, userId: String, subscriptionId: String, direct: Boolean?, event: OutcomeEvent)
 }
