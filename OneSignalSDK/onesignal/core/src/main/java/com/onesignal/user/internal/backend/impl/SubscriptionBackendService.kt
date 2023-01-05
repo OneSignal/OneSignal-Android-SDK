eb0b946205cc0ab45e1c071af6aec1906a0f52a8
Brian Smith <briansmith@onesignal.com>
2023-01-05 13:58:43 -0500

    [User Model] Subscription Already Exists * Updates to support creating a subscription when the subscription already exists for the owner.

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/user/internal/backend/impl/SubscriptionBackendService.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/user/internal/backend/impl/SubscriptionBackendService.kt
@@ -11,7 +11,7 @@ internal class SubscriptionBackendService(
     private val _httpClient: IHttpClient
 ) : ISubscriptionBackendService {
 
-    override suspend fun createSubscription(appId: String, aliasLabel: String, aliasValue: String, subscription: SubscriptionObject): String {
+    override suspend fun createSubscription(appId: String, aliasLabel: String, aliasValue: String, subscription: SubscriptionObject): String? {
         val requestJSON = JSONObject()
             .put("subscription", JSONConverter.convertToJSON(subscription))
             .put("retain_previous_owner", true)
@@ -19,33 +19,13 @@ internal class SubscriptionBackendService(
         val response = _httpClient.post("apps/$appId/users/by/$aliasLabel/$aliasValue/subscriptions", requestJSON)
 
         if (!response.isSuccess) {
-            // TODO: Temporary work around code until resolved on backend
-            if (response.statusCode == 500 && response.payload != null) {
-                val payload = JSONObject(response.payload!!)
-                if (payload.getBoolean("success") || payload.getJSONObject("subscription").has("id")) {
-                    val responseJSON = JSONObject(response.payload!!)
-                    val subscriptionJSON = responseJSON.safeJSONObject("subscription")
-                    if (subscriptionJSON == null || !subscriptionJSON.has("id")) {
-                        throw BackendException(response.statusCode, response.payload)
-                    }
-
-                    return subscriptionJSON.getString("id")
-                }
-            } else if (response.statusCode == 400 && response.payload != null) {
-                val errors = JSONObject(response.payload!!)
-                    .getJSONArray("errors")
-                if (errors.length() > 0 && errors.getJSONObject(0).getString("title").contains("Subscription already belongs to target user")) {
-                    throw BackendException(409, response.payload)
-                }
-            }
-            // TODO: End Temporary work around code until resolved on backend
             throw BackendException(response.statusCode, response.payload)
         }
 
         val responseJSON = JSONObject(response.payload!!)
         val subscriptionJSON = responseJSON.safeJSONObject("subscription")
         if (subscriptionJSON == null || !subscriptionJSON.has("id")) {
-            throw BackendException(response.statusCode, response.payload)
+            return null
         }
 
         return subscriptionJSON.getString("id")
