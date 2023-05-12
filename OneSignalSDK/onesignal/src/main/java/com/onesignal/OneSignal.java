95db3baf5fa966e6847df5ee7531b4bc58d4f32b
Josh Kasten <josh@onesignal.com>
2023-05-11 21:40:36 -0400

    Release commit 4.8.6

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OneSignal.java
@@ -431,7 +431,7 @@ public class OneSignal {
    private static TrackAmazonPurchase trackAmazonPurchase;
    private static TrackFirebaseAnalytics trackFirebaseAnalytics;
 
-   private static final String VERSION = "040805";
+   private static final String VERSION = "040806";
    public static String getSdkVersionRaw() {
       return VERSION;
    }
