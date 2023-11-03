3d33c62f7762d81f064781ac88825ae0053a7192
Jenna Antilla <46546946+jennantilla@users.noreply.github.com>
2023-11-03 15:15:55 -0700

    Update version for v5.0.4 release

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/OneSignalUtils.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/OneSignalUtils.kt
@@ -6,7 +6,7 @@ object OneSignalUtils {
     /**
      * The version of this SDK.
      */
-    const val SDK_VERSION: String = "050003"
+    const val SDK_VERSION: String = "050004"
 
     fun isValidEmail(email: String): Boolean {
         if (email.isEmpty()) {
