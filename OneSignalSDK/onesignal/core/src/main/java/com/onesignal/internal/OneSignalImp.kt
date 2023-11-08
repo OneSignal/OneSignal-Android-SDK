986adfa4f6f150c77f77dc102492a38dba94a109
Brian Smith <briansmith@onesignal.com>
2023-11-08 07:23:42 -0500

    Rename OneSignalImplTests.kt -> OneSignalImpTests Fix linting errors with OneSignalImp and OneSignalImpTests

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/internal/OneSignalImp.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/internal/OneSignalImp.kt
@@ -247,10 +247,11 @@ internal class OneSignalImp : IOneSignal, IServiceProvider {
             startupService!!.bootstrap()
 
             if (forceCreateUser || !identityModelStore!!.model.hasProperty(IdentityConstants.ONESIGNAL_ID)) {
-                val legacyPlayerId = preferencesService!!.getString(
-                    PreferenceStores.ONESIGNAL,
-                    PreferenceOneSignalKeys.PREFS_LEGACY_PLAYER_ID
-                )
+                val legacyPlayerId =
+                    preferencesService!!.getString(
+                        PreferenceStores.ONESIGNAL,
+                        PreferenceOneSignalKeys.PREFS_LEGACY_PLAYER_ID,
+                    )
                 if (legacyPlayerId == null) {
                     Logging.debug("initWithContext: creating new device-scoped user")
                     createAndSwitchToNewUser()
@@ -290,7 +291,7 @@ internal class OneSignalImp : IOneSignal, IServiceProvider {
                         configModel!!.pushSubscriptionId = legacyPlayerId
                         subscriptionModelStore!!.add(
                             pushSubscriptionModel,
-                            ModelChangeTags.NO_PROPOGATE
+                            ModelChangeTags.NO_PROPOGATE,
                         )
                         suppressBackendOperation = true
                     }
@@ -301,13 +302,13 @@ internal class OneSignalImp : IOneSignal, IServiceProvider {
                         LoginUserFromSubscriptionOperation(
                             configModel!!.appId,
                             identityModelStore!!.model.onesignalId,
-                            legacyPlayerId
+                            legacyPlayerId,
                         ),
                     )
                     preferencesService!!.saveString(
                         PreferenceStores.ONESIGNAL,
                         PreferenceOneSignalKeys.PREFS_LEGACY_PLAYER_ID,
-                        null
+                        null,
                     )
                 }
             } else {
@@ -315,8 +316,8 @@ internal class OneSignalImp : IOneSignal, IServiceProvider {
                 operationRepo!!.enqueue(
                     RefreshUserOperation(
                         configModel!!.appId,
-                        identityModelStore!!.model.onesignalId
-                    )
+                        identityModelStore!!.model.onesignalId,
+                    ),
                 )
             }
 
