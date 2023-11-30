b3ae278db9d43ac4e70ac25ac4ffa860c3001048
Nan <nanftw@gmail.com>
2023-11-29 23:17:14 -0800

    Don't hydrate device info for a push subscription model
    When hydrating an existing push subscription model, use existing local device-scoped information instead of remote information for:
    - sdk
    - deviceOS
    - carrier
    - appVersion
    This information should always come from the local device. The reason for this change is that on a new session, we may detect a change to one of these properties and we do not want to overwrite it with old remote data from the get_user response.

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/user/internal/subscriptions/SubscriptionModelStore.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/user/internal/subscriptions/SubscriptionModelStore.kt
@@ -1,8 +1,34 @@
 package com.onesignal.user.internal.subscriptions
 
+import com.onesignal.common.modeling.ModelChangeTags
 import com.onesignal.common.modeling.SimpleModelStore
 import com.onesignal.core.internal.preferences.IPreferencesService
 
 open class SubscriptionModelStore(prefs: IPreferencesService) : SimpleModelStore<SubscriptionModel>({
     SubscriptionModel()
-}, "subscriptions", prefs)
+}, "subscriptions", prefs) {
+    override fun replaceAll(
+        models: List<SubscriptionModel>,
+        tag: String,
+    ) {
+        if (tag != ModelChangeTags.HYDRATE) {
+            return super.replaceAll(models, tag)
+        }
+        // When hydrating an existing push subscription model, use existing device properties
+        synchronized(models) {
+            for (model in models) {
+                if (model.type == SubscriptionType.PUSH) {
+                    val existingPushModel = get(model.id)
+                    if (existingPushModel != null) {
+                        model.sdk = existingPushModel.sdk
+                        model.deviceOS = existingPushModel.deviceOS
+                        model.carrier = existingPushModel.carrier
+                        model.appVersion = existingPushModel.appVersion
+                    }
+                    break
+                }
+            }
+            super.replaceAll(models, tag)
+        }
+    }
+}
