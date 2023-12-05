07a3647e8e7e163ac6a6c5f94776fa94884633ce
Nan <nanftw@gmail.com>
2023-12-04 19:58:44 -0800

    Reset session model on cold starts
    - On cold starts, the app may not be in the background for over 30 seconds, which is when  the session time is sent and the session model's `isValid` property is set to false.
    - Therefore, after the session model is read from cache, reset it's `isValid` property to `false` in order to drive the creation of a new session on cold starts.

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/session/internal/session/SessionModelStore.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/session/internal/session/SessionModelStore.kt
@@ -5,5 +5,19 @@ import com.onesignal.common.modeling.SingletonModelStore
 import com.onesignal.core.internal.preferences.IPreferencesService
 
 open class SessionModelStore(prefs: IPreferencesService) : SingletonModelStore<SessionModel>(
-    SimpleModelStore({ SessionModel() }, "session", prefs),
-)
+    SimpleModelStore(
+        { SessionModel() },
+        "session",
+        prefs,
+    ),
+) {
+    /**
+     * Called after the underlying ModelStore is initialized.
+     * On cold starts, reset the session validity property to drive a new session.
+     */
+    init {
+        if (!store.list().isEmpty()) {
+            model.isValid = false
+        }
+    }
+}
