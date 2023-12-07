9e3c8b8c51bb0e3a2ab06ed34c76c074ba50c99c
jinliu9508 <jinliu9508@gmail.com>
2023-12-07 13:25:06 -0500

    call bindService with applicationContext

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/purchases/impl/TrackGooglePurchase.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/purchases/impl/TrackGooglePurchase.kt
@@ -132,7 +132,7 @@ internal class TrackGooglePurchase(
             mServiceConn = serviceConn
             val serviceIntent = Intent("com.android.vending.billing.InAppBillingService.BIND")
             serviceIntent.setPackage("com.android.vending")
-            _applicationService.appContext.bindService(serviceIntent, serviceConn, Context.BIND_AUTO_CREATE)
+            _applicationService.appContext.applicationContext.bindService(serviceIntent, serviceConn, Context.BIND_AUTO_CREATE)
         } else if (mIInAppBillingService != null) {
             queryBoughtItems()
         }
