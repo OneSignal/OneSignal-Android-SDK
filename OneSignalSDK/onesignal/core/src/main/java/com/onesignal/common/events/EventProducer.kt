a74f95ddd33d484a75eee4c775d3a012b803ed23
jinliu9508 <jinliu9508@gmail.com>
2024-01-26 15:19:45 -0500

    Change more loops with callback calls and add unit test

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/events/EventProducer.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/events/EventProducer.kt
@@ -60,7 +60,8 @@ open class EventProducer<THandler> : IEventNotifier<THandler> {
      */
     fun fireOnMain(callback: (THandler) -> Unit) {
         suspendifyOnMain {
-            for (s in subscribers) {
+            val localList = subscribers.toList()
+            for (s in localList) {
                 callback(s)
             }
         }
@@ -73,7 +74,8 @@ open class EventProducer<THandler> : IEventNotifier<THandler> {
      * @param callback The callback will be invoked for each subscribed handler, allowing you to call the handler.
      */
     suspend fun suspendingFire(callback: suspend (THandler) -> Unit) {
-        for (s in subscribers) {
+        val localList = subscribers.toList()
+        for (s in localList) {
             callback(s)
         }
     }
@@ -86,7 +88,8 @@ open class EventProducer<THandler> : IEventNotifier<THandler> {
      */
     suspend fun suspendingFireOnMain(callback: suspend (THandler) -> Unit) {
         withContext(Dispatchers.Main) {
-            for (s in subscribers) {
+            val localList = subscribers.toList()
+            for (s in localList) {
                 callback(s)
             }
         }
