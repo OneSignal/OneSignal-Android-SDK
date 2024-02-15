ea9c640ad10cea78e1b1df96d63dae0dfda88436
Josh Kasten <josh@onesignal.com>
2024-02-15 17:20:56 -0500

    fixed FAIL_PAUSE_OPREPO not updated in test
    PR #1900 intorduced FAIL_PAUSE_OPREPO but never updated the test

--- OneSignalSDK/onesignal/core/src/test/java/com/onesignal/user/internal/operations/LoginUserOperationExecutorTests.kt
+++ OneSignalSDK/onesignal/core/src/test/java/com/onesignal/user/internal/operations/LoginUserOperationExecutorTests.kt
@@ -129,7 +129,7 @@ class LoginUserOperationExecutorTests : FunSpec({
         val response = loginUserOperationExecutor.execute(operations)
 
         // Then
-        response.result shouldBe ExecutionResult.FAIL_NORETRY
+        response.result shouldBe ExecutionResult.FAIL_PAUSE_OPREPO
         coVerify(exactly = 1) { mockUserBackendService.createUser(appId, mapOf(), any(), any()) }
     }
 
