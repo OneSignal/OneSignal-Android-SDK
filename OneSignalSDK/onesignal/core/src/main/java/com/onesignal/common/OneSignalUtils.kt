cfadb6660adfd3b7d2812d48ab41c419ab1b91dd
Nan <nanftw@gmail.com>
2024-02-22 11:56:26 -0800

    release commit 5.1.5

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/OneSignalUtils.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/OneSignalUtils.kt
@@ -6,7 +6,7 @@ object OneSignalUtils {
     /**
      * The version of this SDK.
      */
-    const val SDK_VERSION: String = "050104"
+    const val SDK_VERSION: String = "050105"
 
     fun isValidEmail(email: String): Boolean {
         if (email.isEmpty()) {
