f342567076a0c5ce02c8733529b118bc3159bd12
Jenna Antilla <46546946+jennantilla@users.noreply.github.com>
2024-03-12 16:12:38 -0700

    Add request JWT in user subscription operations

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/user/internal/operations/UpdateSubscriptionOperation.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/user/internal/operations/UpdateSubscriptionOperation.kt
@@ -1,5 +1,6 @@
 package com.onesignal.user.internal.operations
 
+import com.onesignal.OneSignal
 import com.onesignal.common.IDManager
 import com.onesignal.core.internal.operations.GroupComparisonType
 import com.onesignal.core.internal.operations.Operation
@@ -81,13 +82,22 @@ class UpdateSubscriptionOperation() : Operation(SubscriptionOperationExecutor.UP
             setEnumProperty(::status.name, value)
         }
 
+    /**
+     * The jwt token used for the operation that updates a subscription.
+     */
+    var jwt: String?
+        get() = getStringProperty(::jwt.name)
+        private set(value) {
+            setStringProperty(::jwt.name, value!!)
+        }
+
     override val createComparisonKey: String get() = "$appId.User.$onesignalId"
     override val modifyComparisonKey: String get() = "$appId.User.$onesignalId.Subscription.$subscriptionId"
     override val groupComparisonType: GroupComparisonType = GroupComparisonType.ALTER
     override val canStartExecute: Boolean get() = !IDManager.isLocalId(onesignalId) && !IDManager.isLocalId(onesignalId)
     override val applyToRecordId: String get() = subscriptionId
 
-    constructor(appId: String, onesignalId: String, subscriptionId: String, type: SubscriptionType, enabled: Boolean, address: String, status: SubscriptionStatus) : this() {
+    constructor(appId: String, onesignalId: String, subscriptionId: String, type: SubscriptionType, enabled: Boolean, address: String, status: SubscriptionStatus, jwt: String? = null) : this() {
         this.appId = appId
         this.onesignalId = onesignalId
         this.subscriptionId = subscriptionId
@@ -95,6 +105,7 @@ class UpdateSubscriptionOperation() : Operation(SubscriptionOperationExecutor.UP
         this.enabled = enabled
         this.address = address
         this.status = status
+        this.jwt = jwt
     }
 
     override fun translateIds(map: Map<String, String>) {
