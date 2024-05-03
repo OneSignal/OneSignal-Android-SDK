37487e0f76624d19be99fb670f8528821cfeb0b1
Josh Kasten <josh@onesignal.com>
2024-05-02 20:02:42 -0400

    add OneSignal-Install-Id test to HttpClientTest

--- OneSignalSDK/onesignal/core/src/test/java/com/onesignal/core/internal/http/HttpClientTests.kt
+++ OneSignalSDK/onesignal/core/src/test/java/com/onesignal/core/internal/http/HttpClientTests.kt
@@ -21,8 +21,9 @@ class Mocks {
     internal val mockConfigModel = MockHelper.configModelStore()
     internal val response = MockHttpConnectionFactory.MockResponse()
     internal val factory = MockHttpConnectionFactory(response)
+    internal val installIdService = InstallIdService(MockPreferencesService())
     internal val httpClient by lazy {
-        HttpClient(factory, MockPreferencesService(), mockConfigModel, Time(), InstallIdService(MockPreferencesService()))
+        HttpClient(factory, MockPreferencesService(), mockConfigModel, Time(), installIdService)
     }
 }
 
@@ -51,7 +52,7 @@ class HttpClientTests : FunSpec({
         response.throwable should beInstanceOf<TimeoutCancellationException>()
     }
 
-    test("SDKHeader is included in all requests") {
+    test("SDK Headers are included in all requests") {
         // Given
         val mocks = Mocks()
         val httpClient = mocks.httpClient
@@ -66,6 +67,7 @@ class HttpClientTests : FunSpec({
         // Then
         for (connection in mocks.factory.connections) {
             connection.getRequestProperty("SDK-Version") shouldBe "onesignal/android/${OneSignalUtils.SDK_VERSION}"
+            connection.getRequestProperty("OneSignal-Install-Id") shouldBe mocks.installIdService.getId().toString()
         }
     }
 
