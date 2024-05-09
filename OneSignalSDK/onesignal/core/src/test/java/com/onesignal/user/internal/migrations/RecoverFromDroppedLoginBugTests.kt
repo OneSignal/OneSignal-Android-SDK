b04a96a30ac6f7a001b16f359aa112d4ecaf203c
Josh Kasten <josh@onesignal.com>
2024-05-09 19:27:55 -0400

    proving onOperationRepoLoaded doesn't always fire
    Add test to prove onOperationRepoLoaded doesn't always fire.
    Refactored test to make it easier to share mocks between tests.

--- OneSignalSDK/onesignal/core/src/test/java/com/onesignal/user/internal/migrations/RecoverFromDroppedLoginBugTests.kt
+++ OneSignalSDK/onesignal/core/src/test/java/com/onesignal/user/internal/migrations/RecoverFromDroppedLoginBugTests.kt
@@ -15,44 +15,84 @@ import io.mockk.mockk
 import io.mockk.runs
 import io.mockk.spyk
 import io.mockk.verify
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withTimeout
+
+private class Mocks {
+    val operationModelStore: OperationModelStore =
+        run {
+            val mockOperationModelStore = mockk<OperationModelStore>()
+            every { mockOperationModelStore.loadOperations() } just runs
+            every { mockOperationModelStore.list() } returns listOf()
+            mockOperationModelStore
+        }
+    val configModelStore = mockk<ConfigModelStore>()
+    val operationRepo =
+        spyk(
+            OperationRepo(
+                listOf(),
+                operationModelStore,
+                configModelStore,
+                Time(),
+                ExecutorMocks.getNewRecordState(configModelStore),
+            ),
+        )
+
+    val recovery = spyk(RecoverFromDroppedLoginBug(operationRepo, MockHelper.identityModelStore(), configModelStore))
+}
 
 class RecoverFromDroppedLoginBugTests : FunSpec({
-    test("ensure RecoverFromDroppedLoginBug receive onOperationRepoLoaded callback from operationRepo") {
+    test("ensure onOperationRepoLoaded callback fires from operationRepo") {
         // Given
-        val mockOperationModelStore = mockk<OperationModelStore>()
-        val mockConfigModelStore = mockk<ConfigModelStore>()
-        val operationRepo =
-            spyk(
-                OperationRepo(
-                    listOf(),
-                    mockOperationModelStore,
-                    mockConfigModelStore,
-                    Time(),
-                    ExecutorMocks.getNewRecordState(mockConfigModelStore),
-                ),
-            )
-        every { mockOperationModelStore.loadOperations() } just runs
-        every { mockOperationModelStore.list() } returns listOf()
-        val recovery = spyk(RecoverFromDroppedLoginBug(operationRepo, MockHelper.identityModelStore(), mockConfigModelStore))
+        val mocks = Mocks()
 
         // When
-        recovery.start()
+        mocks.recovery.start()
         val waiter = Waiter()
-        operationRepo.addOperationLoadedListener(
+        mocks.operationRepo.addOperationLoadedListener(
             object : IOperationRepoLoadedListener {
                 override fun onOperationRepoLoaded() {
                     waiter.wake()
                 }
             },
         )
-        operationRepo.start()
+        mocks.operationRepo.start()
         // Waiting here ensures recovery.onOperationRepoLoaded() is called consistently
         waiter.waitForWake()
 
         // Then
         verify(exactly = 1) {
-            operationRepo.subscribe(recovery)
-            recovery.onOperationRepoLoaded()
+            mocks.operationRepo.subscribe(mocks.recovery)
+            mocks.recovery.onOperationRepoLoaded()
+        }
+    }
+
+    test("ensure onOperationRepoLoaded callback fires from operationRepo, even if started first") {
+        // Given
+        val mocks = Mocks()
+
+        // When
+        mocks.operationRepo.start()
+        // give operation repo some time to fully initialize
+        delay(200)
+
+        mocks.recovery.start()
+
+        val waiter = Waiter()
+        mocks.operationRepo.addOperationLoadedListener(
+            object : IOperationRepoLoadedListener {
+                override fun onOperationRepoLoaded() {
+                    waiter.wake()
+                }
+            },
+        )
+        // Waiting here ensures recovery.onOperationRepoLoaded() is called consistently
+        withTimeout(1_000) { waiter.waitForWake() }
+
+        // Then
+        verify(exactly = 1) {
+            mocks.operationRepo.subscribe(mocks.recovery)
+            mocks.recovery.onOperationRepoLoaded()
         }
     }
 })
