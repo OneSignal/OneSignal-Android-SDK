a23b921f239a4a94b2cb13e98b495ab19d53e865
jinliu9508 <jinliu9508@gmail.com>
2024-06-25 18:45:02 -0400

    improve code cleanliness

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/startup/StartupService.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/startup/StartupService.kt
@@ -8,20 +8,16 @@ import kotlinx.coroutines.newSingleThreadContext
 internal class StartupService(
     private val services: ServiceProvider,
 ) {
-    private var coroutineScope = CoroutineScope(newSingleThreadContext(name = "ServiceProvider"))
+    private val coroutineScope = CoroutineScope(newSingleThreadContext(name = "StartupService"))
 
     fun bootstrap() {
-        for (bootstrapService in services.getAllServices<IBootstrapService>()) {
-            bootstrapService.bootstrap()
-        }
+        services.getAllServices<IBootstrapService>().forEach { it.bootstrap() }
     }
 
     // schedule to start all startable services in a separate thread
     fun scheduleStart() {
         coroutineScope.launch {
-            for (service in services.getAllServices<IStartableService>()) {
-                service.start()
-            }
+            services.getAllServices<IStartableService>().forEach { it.start() }
         }
     }
 }
