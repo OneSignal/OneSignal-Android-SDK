18dd0d55f2d38399abd1c2d58c60321e94af6966
jinliu9508 <jinliu9508@gmail.com>
2024-07-26 13:53:00 -0400

    use thread instead of coroutine

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/startup/StartupService.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/startup/StartupService.kt
@@ -1,23 +1,18 @@
 package com.onesignal.core.internal.startup
 
 import com.onesignal.common.services.ServiceProvider
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
 
 internal class StartupService(
     private val services: ServiceProvider,
 ) {
-    private val coroutineScope = CoroutineScope(newSingleThreadContext(name = "StartupService"))
-
     fun bootstrap() {
         services.getAllServices<IBootstrapService>().forEach { it.bootstrap() }
     }
 
     // schedule to start all startable services in a separate thread
     fun scheduleStart() {
-        coroutineScope.launch {
+        Thread {
             services.getAllServices<IStartableService>().forEach { it.start() }
-        }
+        }.start()
     }
 }
