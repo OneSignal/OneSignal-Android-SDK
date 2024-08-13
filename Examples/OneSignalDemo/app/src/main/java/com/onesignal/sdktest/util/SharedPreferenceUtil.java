8e98b1b1377fcedd9d27eb58673cb35fa1b79e68
jinliu9508 <jinliu9508@gmail.com>
2024-08-12 21:59:41 -0400

    Manually test with a specific app in staging

--- Examples/OneSignalDemo/app/src/main/java/com/onesignal/sdktest/util/SharedPreferenceUtil.java
+++ Examples/OneSignalDemo/app/src/main/java/com/onesignal/sdktest/util/SharedPreferenceUtil.java
@@ -24,7 +24,7 @@ public class SharedPreferenceUtil {
     }
 
     public static String getOneSignalAppId(Context context) {
-        return getSharedPreference(context).getString(OS_APP_ID_SHARED_PREF, "77e32082-ea27-42e3-a898-c72e141824ef");
+        return getSharedPreference(context).getString(OS_APP_ID_SHARED_PREF, "0139bd6f-451f-438c-8886-4e0f0fe3a085");
     }
 
     public static boolean getUserPrivacyConsent(Context context) {
