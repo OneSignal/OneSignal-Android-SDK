73a5e61d933d88ab29550d3dcb896c08eb8ced2d
Rodrigo Gomez Palacio <rodrigo@onesignal.com>
2024-08-23 18:27:21 -0500

    Add offset, retryCount, & secondsSinceAppOpen to OptionalHeaderValues
    Motivation: support passing values as headers

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/http/impl/OptionalHeaderValues.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/http/impl/OptionalHeaderValues.kt
@@ -1,5 +1,8 @@
 package com.onesignal.core.internal.http.impl
 
 data class OptionalHeaderValues(
+    var offset: Long? = null,
+    var retryCount: Int? = null,
+    var secondsSinceAppOpen: Long? = null,
     var cacheKey: String? = null,
 )
