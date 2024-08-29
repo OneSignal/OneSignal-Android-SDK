bc44d605117ec1282e1f392fd54e3717f241f0a2
Rodrigo Gomez Palacio <rodrigo@onesignal.com>
2024-08-29 17:39:20 -0500

    ConsistencyManager interface & implementation
    Manages (kafka) offsets that function as read-your-write tokens for more accurate segment membership calculation. The manager works based on conditions & offsets.
    
    Offsets are stored in a nested map indexed by a unique id (e.g. `onesignalId`) and offset key (e.g. `USER_UPDATE`).
    
    This allows us to track offsets on a per-user basis (e.g. handle switching users).
    
    Conditions work by creating a blocking mechanism with customizable offset retrieval until a pre-defined condition is met (e.g. at least two specific offsets are available).
    
    ICondition interface: allows extensibility for future applications to control offset blocking mechanism in consistency use-cases.

---
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/consistency/models/IConsistencyManager.kt
@@ -0,0 +1,26 @@
+package com.onesignal.common.consistency.models
+
+import kotlinx.coroutines.CompletableDeferred
+
+interface IConsistencyManager {
+    /**
+     * Set method to update the offset based on the key.
+     * Params:
+     *  id: String - the index of the offset map (e.g., onesignalId)
+     *  key: OffsetKey - corresponds to the operation for which we have a read-your-write token
+     *  value: Long? - the offset (read-your-write token)
+     */
+    suspend fun setOffset(
+        id: String,
+        key: IConsistencyKeyEnum,
+        value: Long?,
+    )
+
+    /**
+     * Register a condition with its corresponding deferred action. Returns a deferred condition.
+     * Params:
+     *  condition: ICondition - the condition to be registered
+     * Returns: CompletableDeferred<Long?> - a deferred action that completes when the condition is met
+     */
+    suspend fun registerCondition(condition: ICondition): CompletableDeferred<Long?>
+}
---
+++ OneSignalSDK/onesignal/core/src/test/java/com/onesignal/common/consistency/ConsistencyManagerTests.kt
@@ -0,0 +1,86 @@
+package com.onesignal.common.consistency.impl
+
+import com.onesignal.common.consistency.enums.IamFetchOffsetKey
+import com.onesignal.common.consistency.models.ICondition
+import com.onesignal.common.consistency.models.IConsistencyKeyEnum
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
+
+class ConsistencyManagerTests : FunSpec({
+
+    lateinit var consistencyManager: ConsistencyManager
+
+    beforeAny {
+        consistencyManager = ConsistencyManager()
+    }
+
+    test("setOffset updates the offset correctly") {
+        runTest {
+            // Given
+            val id = "test_id"
+            val key = IamFetchOffsetKey.USER_UPDATE
+            val value = 123L
+
+            consistencyManager.setOffset(id, key, value)
+
+            val condition = TestMetCondition(mapOf(id to mapOf(key to value)))
+            val deferred = consistencyManager.registerCondition(condition)
+            val result = deferred.await()
+
+            result shouldBe value
+        }
+    }
+
+    test("registerCondition completes when condition is met") {
+        runTest {
+            // Given
+            val id = "test_id"
+            val key = IamFetchOffsetKey.USER_UPDATE
+            val value = 123L
+
+            // Set an offset to meet the condition
+            consistencyManager.setOffset(id, key, value)
+
+            val condition = TestMetCondition(mapOf(id to mapOf(key to value)))
+            val deferred = consistencyManager.registerCondition(condition)
+
+            deferred.await()
+            deferred.isCompleted shouldBe true
+        }
+    }
+
+    test("registerCondition does not complete when condition is not met") {
+        runTest {
+            val condition = TestUnmetCondition()
+            val deferred = consistencyManager.registerCondition(condition)
+
+            consistencyManager.setOffset("id", IamFetchOffsetKey.USER_UPDATE, 123L)
+            deferred.isCompleted shouldBe false
+        }
+    }
+}) {
+    // Mock implementation of ICondition that simulates a condition that isn't met
+    private class TestUnmetCondition : ICondition {
+        override fun isMet(offsets: Map<String, Map<IConsistencyKeyEnum, Long?>>): Boolean {
+            return false // Always returns false to simulate an unmet condition
+        }
+
+        override fun getNewestOffset(offsets: Map<String, Map<IConsistencyKeyEnum, Long?>>): Long? {
+            return null
+        }
+    }
+
+    // Mock implementation of ICondition for cases where the condition is met
+    private class TestMetCondition(
+        private val expectedOffsets: Map<String, Map<IConsistencyKeyEnum, Long?>>,
+    ) : ICondition {
+        override fun isMet(offsets: Map<String, Map<IConsistencyKeyEnum, Long?>>): Boolean {
+            return offsets == expectedOffsets
+        }
+
+        override fun getNewestOffset(offsets: Map<String, Map<IConsistencyKeyEnum, Long?>>): Long? {
+            return expectedOffsets.values.firstOrNull()?.values?.firstOrNull()
+        }
+    }
+}
