ccd3467b8f6e6f965cdd4176d060e458e11f0f4b
Rodrigo Gomez Palacio <rodrigo@onesignal.com>
2024-09-23 14:42:41 -0500

    `IamFetchReadyCondition` logic nit & better comments
    Motivation: add details explaining logic.
    
    We should also resolve the condition immediately if one of the tokens is equal to the `RYW_TOKEN_NOT_PROVIDED` const.
    
    We DON'T want to block IAMs if we turn off the feature or a regression occurs that excludes the token from the endpoint response body.

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/consistency/enums/IamFetchRywTokenKey.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/consistency/enums/IamFetchRywTokenKey.kt
@@ -4,6 +4,7 @@ import com.onesignal.common.consistency.models.IConsistencyKeyEnum
 
 /**
  * Each enum is a key that we use to keep track of read-your-write tokens.
+ * Although the enums are named with "UPDATE", they serve as keys for tokens from both PATCH & POST
  */
 enum class IamFetchRywTokenKey : IConsistencyKeyEnum {
     USER_UPDATE,
--- OneSignalSDK/onesignal/in-app-messages/src/main/java/com/onesignal/inAppMessages/internal/IamFetchReadyCondition.kt
+++ OneSignalSDK/onesignal/in-app-messages/src/main/java/com/onesignal/inAppMessages/internal/IamFetchReadyCondition.kt
@@ -1,6 +1,7 @@
 package com.onesignal.inAppMessages.internal
 
 import com.onesignal.common.consistency.enums.IamFetchRywTokenKey
+import com.onesignal.common.consistency.impl.ConsistencyManager
 import com.onesignal.common.consistency.models.ICondition
 import com.onesignal.common.consistency.models.IConsistencyKeyEnum
 
@@ -17,6 +18,29 @@ class IamFetchReadyCondition(
         val tokenMap = indexedTokens[id] ?: return false
         val userUpdateTokenSet = tokenMap[IamFetchRywTokenKey.USER_UPDATE] != null
         val subscriptionUpdateTokenSet = tokenMap[IamFetchRywTokenKey.SUBSCRIPTION_UPDATE] != null
+
+        /**
+         * If the RYW token was not included in one of the 4 endpoints (PATCH & POST * user,
+         * subscription), we should resolve the condition so as to not block IAM fetch unnecessarily
+         * This can happen if there is a regression where we stop including the tokens in the res
+         * body or we turn off the feature temporarily
+         */
+        val subscriptionOrUserTokenWasNotSet =
+            tokenMap[IamFetchRywTokenKey.SUBSCRIPTION_UPDATE] == ConsistencyManager.RYW_TOKEN_NOT_PROVIDED ||
+                tokenMap[IamFetchRywTokenKey.USER_UPDATE] == ConsistencyManager.RYW_TOKEN_NOT_PROVIDED
+
+        if (subscriptionOrUserTokenWasNotSet) {
+            return true
+        }
+
+        /**
+         * We always update the session count so we know we will have a userUpdateToken. We don't
+         * necessarily make a subscriptionUpdate call on every session. The following logic
+         * is written in a way so that if somehow the subscriptionUpdateToken is set *before* the
+         * userUpdateToken, we will wait for the userUpdateToken to also be set. This is because
+         * we know that a userUpdate call was made and both user & subscription properties are
+         * considered during segment calculations.
+         */
         return (userUpdateTokenSet && subscriptionUpdateTokenSet) || userUpdateTokenSet
     }
 
