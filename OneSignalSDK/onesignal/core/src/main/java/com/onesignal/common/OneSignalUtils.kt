0ddd101e4c528f145758c3327a93258858d7a18a
jinliu9508 <jinliu9508@gmail.com>
2024-09-27 15:47:28 -0400

    Release 5.1.22

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/OneSignalUtils.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/OneSignalUtils.kt
@@ -6,7 +6,7 @@ object OneSignalUtils {
     /**
      * The version of this SDK.
      */
-    const val SDK_VERSION: String = "050121"
+    const val SDK_VERSION: String = "050122"
 
     fun isValidEmail(email: String): Boolean {
         if (email.isEmpty()) {
