19a72e8516785f006b06902cf520c2a341c3eaac
jinliu9508 <jinliu9508@gmail.com>
2024-11-22 14:03:50 -0500

    Fix a typo in addUserJwtInvalidatedListener

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/IUserJwtInvalidatedListener.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/IUserJwtInvalidatedListener.kt
@@ -1,7 +1,7 @@
 package com.onesignal
 
 /**
- * Implement this interface and provide an instance to [OneSignal.addUserJwtInvalidatedListner]
+ * Implement this interface and provide an instance to [OneSignal.addUserJwtInvalidatedListener]
  * in order to receive control when the JWT for the current user is invalidated.
  *
  * @see [User JWT Invalidated Event | OneSignal Docs](https://documentation.onesignal.com/docs/identity-verification)
