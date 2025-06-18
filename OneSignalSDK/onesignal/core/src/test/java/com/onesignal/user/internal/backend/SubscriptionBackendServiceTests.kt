213b0cc098b8228b3aa67463ab82055f1c084ddd
Fadi George <fadii925@gmail.com>
2025-06-18 12:09:50 -0700

    add more test cases for delete subscription

--- OneSignalSDK/onesignal/core/src/test/java/com/onesignal/user/internal/backend/SubscriptionBackendServiceTests.kt
+++ OneSignalSDK/onesignal/core/src/test/java/com/onesignal/user/internal/backend/SubscriptionBackendServiceTests.kt
@@ -8,6 +8,7 @@ import com.onesignal.debug.LogLevel
 import com.onesignal.debug.internal.logging.Logging
 import com.onesignal.user.internal.backend.impl.SubscriptionBackendService
 import com.onesignal.user.internal.subscriptions.SubscriptionStatus
+import com.onesignal.user.internal.subscriptions.SubscriptionType
 import io.kotest.assertions.throwables.shouldThrowUnit
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
@@ -103,6 +104,47 @@ class SubscriptionBackendServiceTests : FunSpec({
                     sub.getBoolean("enabled") shouldBe true
                     sub.getInt("notification_types") shouldBe 1
                 },
+                any(),
+            )
+        }
+    }
+
+    test("delete subscription by type and token successfully") {
+        // Given
+        val spyHttpClient = mockk<IHttpClient>()
+        coEvery { spyHttpClient.delete(any(), any()) } returns HttpResponse(200, "")
+        val subscriptionBackendService = SubscriptionBackendService(spyHttpClient)
+
+        // When
+        subscriptionBackendService.deleteSubscription("appId", SubscriptionType.EMAIL, "test@example.com", "jwt-token")
+
+        // Then
+        coVerify {
+            spyHttpClient.delete(
+                "apps/appId/by/type/email/token/test@example.com/subscriptions",
+                any(),
+            )
+        }
+    }
+
+    test("delete subscription throws exception when bad response") {
+        // Given
+        val spyHttpClient = mockk<IHttpClient>()
+        coEvery { spyHttpClient.delete(any(), any()) } returns HttpResponse(404, "NOT FOUND")
+        val subscriptionBackendService = SubscriptionBackendService(spyHttpClient)
+
+        // When
+        val exception =
+            shouldThrowUnit<BackendException> {
+                subscriptionBackendService.deleteSubscription("appId", SubscriptionType.SMS, "+1234567890")
+            }
+
+        // Then
+        exception.statusCode shouldBe 404
+        exception.response shouldBe "NOT FOUND"
+        coVerify {
+            spyHttpClient.delete(
+                "apps/appId/by/type/sms/token/+1234567890/subscriptions",
                 any(),
             )
         }
