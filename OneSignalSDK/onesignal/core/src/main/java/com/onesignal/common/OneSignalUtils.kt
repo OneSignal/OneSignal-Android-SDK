023b5ecb3588d6b35fe218f9a33d5ba55a09f0b3
Josh Kasten <josh@onesignal.com>
2025-06-30 16:05:03 -0500

    Release 5.1.35

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/OneSignalUtils.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/common/OneSignalUtils.kt
@@ -6,7 +6,7 @@ object OneSignalUtils {
     /**
      * The version of this SDK.
      */
-    const val SDK_VERSION: String = "050134"
+    const val SDK_VERSION: String = "050135"
 
     fun isValidEmail(email: String): Boolean {
         if (email.isEmpty()) {
