38f983917bcd401977bfd7e199512c65949b1be2
AR Abdul Azeez <abdul@onesignal.com>
2025-09-18 11:24:43 -0500

    cleaned up tests

--- Examples/OneSignalDemo/app/src/test/java/com.onesignal.sdktest/application/TestApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.onesignal.sdktest.application
-
-class TestApplication : android.app.Application() {
-    override fun onCreate() {
-        // no op; to bypass MainApplication before testing
-    }
-}
