48d42b627d63c44d407f8446adc89d8d0df41cfb
Josh Kasten <josh@onesignal.com>
2025-10-07 19:23:18 -0400

    feat: add Honeycomb test
    Trying out Honeycomb in a test env to see what it logs, including custom attributes.

--- Examples/OneSignalDemo/app/src/main/java/com/onesignal/sdktest/application/MainApplication.java
+++ Examples/OneSignalDemo/app/src/main/java/com/onesignal/sdktest/application/MainApplication.java
@@ -29,9 +29,19 @@ import com.onesignal.user.state.IUserStateObserver;
 import com.onesignal.user.state.UserChangedState;
 import com.onesignal.user.state.UserState;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import io.honeycomb.opentelemetry.android.Honeycomb;
+import io.honeycomb.opentelemetry.android.HoneycombOptions;
+import io.opentelemetry.android.OpenTelemetryRum;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 
 public class MainApplication extends MultiDexApplication {
     private static final int SLEEP_TIME_TO_MIMIC_ASYNC_OPERATION = 2000;
@@ -45,6 +55,25 @@ public class MainApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Map<String, String> attr = new HashMap<>();
+        attr.put("test-setResourceAttributes", "12345");
+
+        HoneycombOptions options = new HoneycombOptions.Builder(this)
+                // Uncomment the line below to send to EU instance. Defaults to US.
+                // .setApiEndpoint("https://api.eu1.honeycomb.io:443")
+                .setApiKey("API_KEY_HERE")
+                .setServiceName("OS-Android-SDK-Test")
+                .setServiceVersion("0.0.1")
+                .setDebug(true)
+                .setResourceAttributes(attr)
+                .setLogRecordProcessor((context, logRecord) ->
+                        // NOTE: This gets call when a crash happens
+                        // TODO: When moving logic to SDK, set app_id, package, org_id, subscription_id, and onesignal_id
+                        logRecord.setAttribute(AttributeKey.stringKey("test-onEmit"), "test-onEmit-value"))
+                .build();
+        OpenTelemetryRum openTelRum = Honeycomb.Companion.configure(this, options);
+
         OneSignal.getDebug().setLogLevel(LogLevel.DEBUG);
 
         // OneSignal Initialization
@@ -54,6 +83,14 @@ public class MainApplication extends MultiDexApplication {
             appId = getString(R.string.onesignal_app_id);
             SharedPreferenceUtil.cacheOneSignalAppId(this, appId);
         }
+
+        /// NOTE: This doesn't add anything to the built in crash handler, even if the span isn't ended.
+//        Tracer tracer = openTelRum.getOpenTelemetry().tracerBuilder("test-tracerBuilder").build();
+//        Span span = tracer.spanBuilder("test-spanBuilder").setAttribute("test_app_id2", "1234").startSpan();
+//        span.end();
+
+        /// NOTE: Doesn't work on it's own, a span is not created by default.
+        // Span.current().setAttribute("test_app_id", "123");
 
         OneSignalNotificationSender.setAppId(appId);
 
