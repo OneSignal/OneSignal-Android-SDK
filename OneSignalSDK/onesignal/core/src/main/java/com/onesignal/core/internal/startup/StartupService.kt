5ef160dd582301caf90820dcb732c60beb62568c
AR Abdul Azeez <abdul@onesignal.com>
2025-12-09 12:58:10 -0500

    Standard test pattern

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/startup/StartupService.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/core/internal/startup/StartupService.kt
@@ -1,18 +1,20 @@
 package com.onesignal.core.internal.startup
 
 import com.onesignal.common.services.ServiceProvider
-import com.onesignal.common.threading.OneSignalDispatchers
+import com.onesignal.common.threading.CoroutineDispatcherProvider
+import com.onesignal.common.threading.DefaultDispatcherProvider
 
 internal class StartupService(
     private val services: ServiceProvider,
+    private val dispatchers: CoroutineDispatcherProvider = DefaultDispatcherProvider(),
 ) {
     fun bootstrap() {
         services.getAllServices<IBootstrapService>().forEach { it.bootstrap() }
     }
 
-    // schedule to start all startable services using OneSignal dispatcher
+    // schedule to start all startable services using the provided dispatcher
     fun scheduleStart() {
-        OneSignalDispatchers.launchOnDefault {
+        dispatchers.launchOnDefault {
             services.getAllServices<IStartableService>().forEach { it.start() }
         }
     }
