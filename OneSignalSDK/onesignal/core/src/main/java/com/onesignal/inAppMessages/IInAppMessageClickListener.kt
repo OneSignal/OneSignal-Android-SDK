6fda5243ff17341d4b69ea8d38e7013f6aaec0a2
abdulraqeeb33 <abdulraqeeb33@gmail.com>
2025-12-28 15:04:54 -0500

    feat: Enable docs (#2507)
    Co-authored-by: AR Abdul Azeez <abdul@onesignal.com>

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/inAppMessages/IInAppMessageClickListener.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/inAppMessages/IInAppMessageClickListener.kt
@@ -1,5 +1,12 @@
 package com.onesignal.inAppMessages
 
+/**
+ * An interface used to process a OneSignal In-App Message the user just clicked on.
+ * Implement this interface and provide an instance to [IInAppMessagesManager.addClickListener]
+ * in order to receive control when an IAM is clicked by the user.
+ *
+ * @see [In-App Messages | OneSignal Docs](https://documentation.onesignal.com/docs/in-app-messages)
+ */
 interface IInAppMessageClickListener {
     /**
      * Fires when a user clicks on a clickable element in the IAM.
