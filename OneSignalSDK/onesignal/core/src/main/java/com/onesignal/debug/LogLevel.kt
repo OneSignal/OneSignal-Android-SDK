7569b81b1f37fca0da46c76dab6bc5360a1ab7d4
abdulraqeeb33 <abdulraqeeb33@gmail.com>
2025-12-28 15:04:54 -0500

    feat: Enable docs (#2507)
    Co-authored-by: AR Abdul Azeez <abdul@onesignal.com>

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/debug/LogLevel.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/debug/LogLevel.kt
@@ -1,12 +1,46 @@
 package com.onesignal.debug
 
+/**
+ * Log level enumeration for controlling the verbosity of OneSignal SDK logging.
+ * Use [IDebugManager.logLevel] to set the desired log level for the SDK.
+ *
+ * Log levels are ordered from least to most verbose:
+ * [NONE] < [FATAL] < [ERROR] < [WARN] < [INFO] < [DEBUG] < [VERBOSE]
+ */
 enum class LogLevel {
+    /**
+     * No logging will be output.
+     */
     NONE,
+
+    /**
+     * Only fatal error messages will be logged.
+     */
     FATAL,
+
+    /**
+     * Error and fatal messages will be logged.
+     */
     ERROR,
+
+    /**
+     * Warning, error, and fatal messages will be logged. This is the default log level.
+     */
     WARN,
+
+    /**
+     * Informational, warning, error, and fatal messages will be logged.
+     */
     INFO,
+
+    /**
+     * Debug, informational, warning, error, and fatal messages will be logged.
+     */
     DEBUG,
+
+    /**
+     * All messages including verbose output will be logged.
+     */
     VERBOSE,
     ;
 
