e2047731ca5bc03f504cc55dda9f288c60e811e6
Fadi George <fadii925@gmail.com>
2026-02-12 10:51:29 -0800

    add back onesignal logo

--- Examples/OneSignalDemoV2/app/src/main/java/com/onesignal/sdktest/ui/main/MainScreen.kt
+++ Examples/OneSignalDemoV2/app/src/main/java/com/onesignal/sdktest/ui/main/MainScreen.kt
@@ -2,14 +2,17 @@ package com.onesignal.sdktest.ui.main
 
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
@@ -28,9 +31,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.onesignal.sdktest.R
 import com.onesignal.sdktest.data.model.NotificationType
 import com.onesignal.sdktest.ui.components.CustomNotificationDialog
 import com.onesignal.sdktest.ui.components.LoadingOverlay
@@ -106,11 +113,15 @@ fun MainScreen(viewModel: MainViewModel) {
         Scaffold(
             topBar = {
                 TopAppBar(
-                    title = { 
-                        Text(
-                            "OneSignal",
-                            color = Color.White
-                        ) 
+                    title = {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Image(
+                                painter = painterResource(id = R.drawable.onesignal_rectangle),
+                                contentDescription = "OneSignal Logo",
+                                modifier = Modifier.height(28.dp),
+                                colorFilter = ColorFilter.tint(Color.White)
+                            )
+                        }
                     },
                     colors = TopAppBarDefaults.topAppBarColors(
                         containerColor = OneSignalRed
