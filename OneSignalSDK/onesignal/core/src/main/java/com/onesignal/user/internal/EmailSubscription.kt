56002897c41320f3b4b9d4ef4d9d6d15686c0a7f
Sherwin Heydarbeygi <sherwin@onesignal.com>
2026-04-21 14:24:37 -0400

    feat: hash PII (email/SMS) in SharedPreferences at rest (#2620)

--- OneSignalSDK/onesignal/core/src/main/java/com/onesignal/user/internal/EmailSubscription.kt
+++ OneSignalSDK/onesignal/core/src/main/java/com/onesignal/user/internal/EmailSubscription.kt
@@ -1,5 +1,6 @@
 package com.onesignal.user.internal
 
+import com.onesignal.common.PIIHasher
 import com.onesignal.user.internal.subscriptions.SubscriptionModel
 import com.onesignal.user.subscriptions.IEmailSubscription
 
@@ -7,5 +8,8 @@ internal class EmailSubscription(
     model: SubscriptionModel,
 ) : Subscription(model), IEmailSubscription {
     override val email: String
-        get() = model.address
+        get() {
+            val address = model.address
+            return if (PIIHasher.isHashed(address)) "" else address
+        }
 }
